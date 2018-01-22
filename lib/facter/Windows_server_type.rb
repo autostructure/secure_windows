807b72612cab809733734ab9e3ef5b129af7086b
Jack Coleman <jack@autostructure.io>
2018-01-22 22:48:08 +0000

    added custom fact windows_server_type

---
+++ lib/facter/Windows_server_type.rb
@@ -0,0 +1,19 @@
+Facter.add(:Windows_server_type) do
+    confine :kernel => :windows
+    setcode do
+      require 'win32/registry'
+    
+      value = nil
+      hive = Win32::Registry::HKEY_LOCAL_MACHINE
+      hive.open('SYSTEM\CurrentControlSet\Control\ProductOptions',  Win32::Registry::KEY_READ | 0x100) do |reg|
+        value = reg['ProductType']
+      end
+      case value
+        when 'LanmanNT'
+          value='windowsdc'
+        else
+          value='MemberServer'
+      end
+      value
+    end
+end
