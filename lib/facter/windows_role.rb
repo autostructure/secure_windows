19bc578aab5c8103df97535be296a86e79ca1212
Jack Coleman <jack@autostructure.io>
2018-01-23 22:05:44 +0000

    test

--- lib/facter/windows_role.rb
+++ lib/facter/windows_role.rb
@@ -7,7 +7,8 @@ Facter.add(:windows_role) do
       require 'facter/util/wmi'
       roles = []
   
-      wmi = Facter::Util::WMI.connect()
+      require 'win32ole'
+      wmi = WIN32OLE.connect("winmgmts:\\\\.\\root\\cimv2")
       wmi.ExecQuery("select ID from Win32_ServerFeature").each do |role|
         roles.push(role.HotFixID)
       end
