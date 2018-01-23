396d13db54a2bc43e465b9813d076406ebaa623f
Jack Coleman <jack@autostructure.io>
2018-01-23 17:58:25 +0000

    first run-through of local_security_policy resources complete

--- manifests/lgpo.pp
+++ manifests/lgpo.pp
@@ -1,9 +1,11 @@
 # This class manages local group policy objects
 #
 # TODO:
-# - Everything
 # - Unit Tests
 # - Handle V-73753 for Hyper-V role. Need a fact for roles.
+# - Member Server Network Access for Domain Servers
+# - Test fact to see if it works for standalone and member servers and two types of DCs
+# - Test if absent is good enough
 #
 class secure_windows::lgpo {
 
@@ -226,9 +228,225 @@ class secure_windows::lgpo {
 
   # V-73753
   # The Create symbolic links user right must only be assigned to the Administrators group.
+  # TODO:
+  # - Hyper-V role gets an additional group.
   local_security_policy { 'Create symbolic links':
     ensure         => 'present',
     policy_setting => 'SeCreateSymbolicLinkPrivilege',
+    policy_type    => 'Privilege Rights',
+    policy_value   => '*S-1-5-32-544',
+  }
+
+  # V-73755
+  # The Debug programs user right must only be assigned to the Administrators group.
+  local_security_policy { 'Debug programs':
+    ensure         => 'present',
+    policy_setting => 'SeDebugPrivilege',
+    policy_type    => 'Privilege Rights',
+    policy_value   => '*S-1-5-32-544',
+  }
+
+  # V-73757
+  # The Deny access to this computer from the network user right on domain controllers must be configured to prevent 
+  # unauthenticated access.
+  if($facts['windows_server_type'] == 'windowsdc') {
+    local_security_policy { 'Deny access to this computer from the network':
+      ensure         => 'present',
+      policy_setting => 'SeDenyNetworkLogonRight',
+      policy_type    => 'Privilege Rights',
+      policy_value   => '*S-1-5-32-546',
+    }
+  }
+
+  # V-73759
+  # The Deny access to this computer from the network user right on member servers must be configured to prevent 
+  # access from highly privileged domain accounts and local accounts on domain systems, and from unauthenticated access on all systems.
+
+
+  # V-73761
+  # The Deny log on as a batch job user right on domain controllers must be configured to prevent unauthenticated access.
+  if($facts['windows_server_type'] == 'windowsdc') {
+    local_security_policy { 'Deny log on as a batch job':
+      ensure         => 'present',
+      policy_setting => 'SeDenyBatchLogonRight',
+      policy_type    => 'Privilege Rights',
+      policy_value   => '*S-1-5-32-546',
+    }
+  }
+
+  # V-73763
+  # The Deny log on as a batch job user right on member servers must be configured to prevent access from highly privileged domain 
+  # accounts on domain systems and from unauthenticated access on all systems.
+
+
+  # V-73765
+  # The Deny log on as a service user right must be configured to include no accounts or groups (blank) on domain controllers.
+  if($facts['windows_server_type'] == 'windowsdc') {
+    local_security_policy { 'Deny log on as a service':
+      ensure         => 'absent',
+    }
+  }
+
+  # V-73767
+  # The Deny log on as a service user right on member servers must be configured to prevent access from highly privileged domain 
+  # accounts on domain systems. No other groups or accounts must be assigned this right.
+
+
+  # V-73769
+  # The Deny log on locally user right on domain controllers must be configured to prevent unauthenticated access.
+  if($facts['windows_server_type'] == 'windowsdc') {
+    local_security_policy { 'Deny log on locally':
+      ensure         => 'present',
+      policy_setting => 'SeDenyInteractiveLogonRight',
+      policy_type    => 'Privilege Rights',
+      policy_value   => '*S-1-5-32-546',
+    }
+  }
+
+  # V-73771
+  # The Deny log on locally user right on member servers must be configured to prevent access from highly privileged domain accounts on 
+  # domain systems and from unauthenticated access on all systems.
+
+
+  # V-73773
+  # The Deny log on through Remote Desktop Services user right on domain controllers must be configured to prevent unauthenticated access.
+  if($facts['windows_server_type'] == 'windowsdc') {
+    local_security_policy { 'Deny log on through Remote Desktop Services':
+      ensure         => 'present',
+      policy_setting => 'SeDenyRemoteInteractiveLogonRight',
+      policy_type    => 'Privilege Rights',
+      policy_value   => '*S-1-5-32-546',
+    }
+  }
+
+  # V-73775
+  # The Deny log on through Remote Desktop Services user right on member servers must be configured to prevent access from highly
+  # privileged domain accounts and all local accounts on domain systems and from unauthenticated access on all systems.
+
+
+  # V-73777
+  # The Enable computer and user accounts to be trusted for delegation user right must only be assigned to the Administrators group on
+  # domain controllers.
+  if($facts['windows_server_type'] == 'windowsdc') {
+    local_security_policy { 'Enable computer and user accounts to be trusted for delegation':
+      ensure         => 'present',
+      policy_setting => 'SeEnableDelegationPrivilege',
+      policy_type    => 'Privilege Rights',
+      policy_value   => '*S-1-5-32-544',
+    }    
+  }
+
+  # V-73779
+  # The Enable computer and user accounts to be trusted for delegation user right must not be assigned to any groups or accounts on member
+  # servers.
+  if($facts['windows_server_type'] == 'MemberServer') {
+    local_security_policy { 'Enable computer and user accounts to be trusted for delegation':
+      ensure         => 'absent',
+    }  
+  }
+
+  # V-73781
+  # The Force shutdown from a remote system user right must only be assigned to the Administrators group.
+  local_security_policy { 'Force shutdown from a remote system':
+    ensure         => 'present',
+    policy_setting => 'SeRemoteShutdownPrivilege',
+    policy_type    => 'Privilege Rights',
+    policy_value   => '*S-1-5-32-544',
+  }
+
+  # V-73783
+  # The Generate security audits user right must only be assigned to Local Service and Network Service.
+  local_security_policy { 'Generate security audits':
+    ensure         => 'present',
+    policy_setting => 'SeAuditPrivilege',
+    policy_type    => 'Privilege Rights',
+    policy_value   => '*S-1-5-19,*S-1-5-20',
+  }
+
+  # V-73785
+  # The Impersonate a client after authentication user right must only be assigned to Administrators, Service, Local Service, and Network
+  # Service.
+  local_security_policy { 'Impersonate a client after authentication':
+    ensure         => 'present',
+    policy_setting => 'SeImpersonatePrivilege',
+    policy_type    => 'Privilege Rights',
+    policy_value   => '*S-1-5-19,*S-1-5-20,*S-1-5-32-544,*S-1-5-6',
+  }
+
+  # V-73787
+  # The Increase scheduling priority user right must only be assigned to the Administrators group.
+  local_security_policy { 'Increase scheduling priority':
+    ensure         => 'present',
+    policy_setting => 'SeIncreaseBasePriorityPrivilege',
+    policy_type    => 'Privilege Rights',
+    policy_value   => '*S-1-5-32-544',
+  }
+
+  # V-73789
+  # The Load and unload device drivers user right must only be assigned to the Administrators group.
+  local_security_policy { 'Load and unload device drivers':
+    ensure         => 'present',
+    policy_setting => 'SeLoadDriverPrivilege',
+    policy_type    => 'Privilege Rights',
+    policy_value   => '*S-1-5-32-544',
+  }
+
+  # V-73791
+  # The Lock pages in memory user right must not be assigned to any groups or accounts.
+  local_security_policy { 'Lock pages in memory':
+    ensure         => 'absent',
+  }
+
+  # V-73793
+  # The Manage auditing and security log user right must only be assigned to the Administrators group.
+  local_security_policy { 'Manage auditing and security log':
+    ensure         => 'present',
+    policy_setting => 'SeSecurityPrivilege',
+    policy_type    => 'Privilege Rights',
+    policy_value   => '*S-1-5-32-544',
+  }
+
+  # V-73795
+  # The Modify firmware environment values user right must only be assigned to the Administrators group.
+  local_security_policy { 'Modify firmware environment values':
+    ensure         => 'present',
+    policy_setting => 'SeSystemEnvironmentPrivilege',
+    policy_type    => 'Privilege Rights',
+    policy_value   => '*S-1-5-32-544',
+  }
+
+  # V-73797
+  # The Perform volume maintenance tasks user right must only be assigned to the Administrators group.
+  local_security_policy { 'Perform volume maintenance tasks':
+    ensure         => 'present',
+    policy_setting => 'SeManageVolumePrivilege',
+    policy_type    => 'Privilege Rights',
+    policy_value   => '*S-1-5-32-544',
+  }
+
+  # V-73799
+  # The Profile single process user right must only be assigned to the Administrators group.
+  local_security_policy { 'Profile single process':
+    ensure         => 'present',
+    policy_setting => 'SeProfileSingleProcessPrivilege',
+    policy_type    => 'Privilege Rights',
+    policy_value   => '*S-1-5-32-544',
+  }
+
+  # V-73801
+  # The Restore files and directories user right must only be assigned to the Administrators group.
+  local_security_policy { 'Restore files and directories':
+    ensure         => 'present',
+    policy_setting => 'SeRestorePrivilege',
+    policy_type    => 'Privilege Rights',
+    policy_value   => '*S-1-5-32-544',
+  }
+
+  # V-73803
+  # The Take ownership of files or other objects user right must only be assigned to the Administrators group.
+  local_security_policy { 'Take ownership of files or other objects':
+    ensure         => 'present',
+    policy_setting => 'SeTakeOwnershipPrivilege',
     policy_type    => 'Privilege Rights',
     policy_value   => '*S-1-5-32-544',
   }
