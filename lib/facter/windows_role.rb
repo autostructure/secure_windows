9e1f911041159f29c61bb2ea94da2c6ddce8b295
Jack Coleman <jack@autostructure.io>
2018-01-24 14:55:18 -0500

    rubocop

--- lib/facter/windows_role.rb
+++ lib/facter/windows_role.rb
@@ -6,17 +6,16 @@
 require 'facter'
 
 Facter.add(:windows_role) do
-    confine :operatingsystem => :windows
-    setcode do
-  
-      roles = []
-  
-      require 'win32ole'
-      wmi = WIN32OLE.connect("winmgmts:\\\\.\\root\\cimv2")
-      wmi.ExecQuery("select ID from Win32_ServerFeature").each do |role|
-        roles.push(role.ID)
-      end
-  
-      roles.sort.join(',')
+  confine operatingsystem: :windows
+  setcode do
+    roles = []
+
+    require 'win32ole'
+    wmi = WIN32OLE.connect("winmgmts:\\\\.\\root\\cimv2")
+    wmi.ExecQuery("select ID from Win32_ServerFeature").each do |role|
+      roles.push(role.ID)
     end
+
+    roles.sort.join(',')
   end
+end
