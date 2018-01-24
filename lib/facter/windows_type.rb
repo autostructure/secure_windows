d574621ca4b8ded11762d44b5ccc095f970f851b
Jack Coleman <jack@autostructure.io>
2018-01-24 14:55:18 -0500

    rubocop

--- lib/facter/windows_type.rb
+++ lib/facter/windows_type.rb
@@ -1,17 +1,16 @@
 require 'facter'
 
 Facter.add(:windows_type) do
-    confine :operatingsystem => :windows
-    setcode do
-  
-      roles = []
-  
-      require 'win32ole'
-      wmi = WIN32OLE.connect("winmgmts:\\\\.\\root\\cimv2")
-      wmi.ExecQuery("select * from Win32_ComputerSystem").each do |role|
-        roles.push(role.DomainRole)
-      end
-  
-      roles.sort.join(',')
+  confine operatingsystem: :windows
+  setcode do
+    roles = []
+
+    require 'win32ole'
+    wmi = WIN32OLE.connect("winmgmts:\\\\.\\root\\cimv2")
+    wmi.ExecQuery("select * from Win32_ComputerSystem").each do |role|
+      roles.push(role.DomainRole)
     end
+
+    roles.sort.join(',')
+  end
 end
