a4b1054025f9606b62e1a75ced65151f45963bd7
Jeffrey Frelick <jeff@autostructure.io>
2018-01-25 09:43:14 -0500

    Revised log deftype

--- manifests/log.pp
+++ manifests/log.pp
@@ -6,12 +6,22 @@
 #
 define secure_windows::log (
   Boolean $enabled = true,
-  String $threatlevel = 'warning',
+  Enum['alert', 'crit', 'debug', 'emerg', 'err', 'info', 'notice', 'warning'] $threatlevel = 'warning',
 ) {
 
   if $enabled {
     # puppetserver.log
-    warning("${facts['fqdn']}: ${title}")
+    case $threatlevel {
+      'alert':   { alert("${facts['fqdn']}: ${title}")   }  # always visible
+      'crit':    { crit("${facts['fqdn']}: ${title}")    }  # always visible
+      'debug':   { debug("${facts['fqdn']}: ${title}")   }  # visible only with -d or --debug
+      'emerg':   { emerg("${facts['fqdn']}: ${title}")   }  # always visible
+      'err':     { err("${facts['fqdn']}: ${title}")     }  # always visible
+      'info':    { info("${facts['fqdn']}: ${title}")    }  # visible only with -v or --verbose or -d or --debug
+      'notice':  { notice("${facts['fqdn']}: ${title}")  }  # always visible
+      'warning': { warning("${facts['fqdn']}: ${title}") }  # always visible
+      default:   { warning("${facts['fqdn']}: ${title}") }
+    }
 
     # puppet agent logging
     notify { "puppetagentlogger_${title}":
