f64b597b8c387078ba4c1a5e4c22f40e19082e30
Jeffrey Frelick <jeff@autostructure.io>
2018-01-25 09:46:14 -0500

    Minor formatting revision.  Log threat level added.

--- manifests/log.pp
+++ manifests/log.pp
@@ -12,15 +12,15 @@ define secure_windows::log (
   if $enabled {
     # puppetserver.log
     case $threatlevel {
-      'alert':   { alert("${facts['fqdn']}: ${title}")   }  # always visible
-      'crit':    { crit("${facts['fqdn']}: ${title}")    }  # always visible
-      'debug':   { debug("${facts['fqdn']}: ${title}")   }  # visible only with -d or --debug
-      'emerg':   { emerg("${facts['fqdn']}: ${title}")   }  # always visible
-      'err':     { err("${facts['fqdn']}: ${title}")     }  # always visible
-      'info':    { info("${facts['fqdn']}: ${title}")    }  # visible only with -v or --verbose or -d or --debug
-      'notice':  { notice("${facts['fqdn']}: ${title}")  }  # always visible
+      'alert':   { alert("${facts['fqdn']}: ${title}") }    # always visible
+      'crit':    { crit("${facts['fqdn']}: ${title}") }     # always visible
+      'debug':   { debug("${facts['fqdn']}: ${title}") }    # visible only with -d or --debug
+      'emerg':   { emerg("${facts['fqdn']}: ${title}") }    # always visible
+      'err':     { err("${facts['fqdn']}: ${title}") }      # always visible
+      'info':    { info("${facts['fqdn']}: ${title}") }     # visible only with -v or --verbose or -d or --debug
+      'notice':  { notice("${facts['fqdn']}: ${title}") }   # always visible
       'warning': { warning("${facts['fqdn']}: ${title}") }  # always visible
-      default:   { warning("${facts['fqdn']}: ${title}") }
+      default:   { warning("${facts['fqdn']}: ${title}") }  # always visible
     }
 
     # puppet agent logging
