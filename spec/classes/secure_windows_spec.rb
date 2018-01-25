3d60bc43d09930a51564d8f1ab804da07a5fa0d4
Jack Coleman <jack@autostructure.io>
2018-01-25 00:57:04 +0000

    Figured out how to merge facts

--- spec/classes/secure_windows_spec.rb
+++ spec/classes/secure_windows_spec.rb
@@ -5,8 +5,9 @@ describe 'secure_windows' do
     on_supported_os.each do |os, facts|
       context "on #{os}" do
         let(:facts) do
-          facts
-          super().merge({ 'windows_type' => '2' })
+          facts.merge({
+            :windows_type => '2',
+          })
         end
 
         print facts
