59a439854f88d1bc39443c56915b365d5d6e9af9
Jeffrey Frelick <jeff@autostructure.io>
2018-01-26 14:20:59 -0500

    more provider coding

--- lib/puppet/provider/policy_template_file_manager.rb
+++ lib/puppet/provider/policy_template_file_manager.rb
@@ -4,9 +4,15 @@
 # secedit /configure /db FileName [/cfg FileName ] [/overwrite][/areas area1 area2...] [/log FileName] [/quiet]
 # %windir%\security\database directory (where %windir% is the drive and path to your Windows directory).
 # secedit /configure /db mysecure.sdb /areas FILESTORE /log %windir% \security\logs\Mysecure.log /verbose.
+confine :osfamily => :windows
 
 dbfile = 'c:\Windows\security\database\secedit.sdb'
 admintemplatefile = 'MSS-Legacy.admx' # MSS-Legacy.admx, MSS-Legacy.adml
-logfile = "c:\\puppet-secedit.log"
-cmdline = "secedit /configure /db #{dbfile} /cfg #{admintemplatefile} /overwrite /log #{logfile}"
-Facter::Core::Execution.exec(cmdline)
+logfile = 'c:\Windows\security\logs\puppet-secedit.log'
+cmdline = "secedit /configure /db #{dbfile} /cfg #{admintemplatefile} /log #{logfile}" # /overwrite ???
+
+if File.exist?(dbfile) && File.exist?(admintemplatefile)
+  Facter::Core::Execution.exec(cmdline)
+else
+  raise Puppet::Error, "Failed to load policy template: #{cmdline}"
+end
