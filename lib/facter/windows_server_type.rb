cb7421fa16bccae84cc1c4c7f1543ffcf42023eb
Bryan Belanger <bryan@autostructure.io>
2018-01-25 22:58:25 -0500

    Again.

---
+++ lib/facter/windows_server_type.rb
@@ -0,0 +1,19 @@
+Facter.add(:windows_server_type) do
+  confine kernel: :windows
+  setcode do
+    require 'win32/registry'
+
+    value = nil
+    hive = Win32::Registry::HKEY_LOCAL_MACHINE
+    hive.open('SYSTEM\CurrentControlSet\Control\ProductOptions', Win32::Registry::KEY_READ | 0x100) do |reg|
+      value = reg['ProductType']
+    end
+    value = case value
+            when 'LanmanNT'
+              'windowsdc'
+            else
+              'MemberServer'
+            end
+    value
+  end
+end
