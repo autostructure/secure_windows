2e7c5fddf22d597b1f15e771795bd90af2d1c672
Jeffrey Frelick <jeff@autostructure.io>
2018-01-29 10:53:07 -0500

    Checked in provider code.

--- lib/puppet/provider/group_policy_template.rb
+++ lib/puppet/provider/group_policy_template.rb
@@ -2,18 +2,18 @@
 # policy_template_file_manager.rb
 #
 # secedit /configure /db FileName [/cfg FileName ] [/overwrite][/areas area1 area2...] [/log FileName] [/quiet]
-# %windir%\security\database directory (where %windir% is the drive and path to your Windows directory).
+# %windir%\security\database drectory (where %windir% is the drive and path to your Windows directory).
 # secedit /configure /db mysecure.sdb /areas FILESTORE /log %windir% \security\logs\Mysecure.log /verbose.
-confine :osfamily => :windows
-defaultfor :operatingsystem => :windows
-confine :exists => "c:\\Windows\\security\\database\\secedit.sdb"
-commands :secedit => "secedit"
+Puppet::Type.type(:group_policy_template).provide(:group_policy_template) do
+  desc "Puppet custom type provider that loads Windows group policy templates."
 
-Puppet::Type.type(:package).provide(:secedit) do
-  desc "puppet windows provider that loads group policy templates."
+  confine :osfamily => :windows
+  defaultfor :operatingsystem => :windows
+  confine :exists => "c:\\Windows\\security\\database\\secedit.sdb"
+  commands :secedit => "secedit"
 
   dbfile = 'c:\Windows\security\database\secedit.sdb'
-  templatefile = 'MSS-Legacy.admx' # MSS-Legacy.admx, MSS-Legacy.adml
+  templatefile = 'c:\tmp\MSS-Legacy.admx' # MSS-Legacy.admx, MSS-Legacy.adml
   logfile = 'c:\Windows\security\logs\puppet-secedit.log'
   _cmdline = "secedit /configure /db #{dbfile} /cfg #{templatefile} /log #{logfile}" # /overwrite ???
 
@@ -30,5 +30,4 @@ Puppet::Type.type(:package).provide(:secedit) do
       raise Puppet::Error, "Failed to load policy template: #{_cmdline}"
     end
   end
-
 end
