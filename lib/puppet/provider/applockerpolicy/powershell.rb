922f24feb25150aa94600c99dd02cb5c9a5b1ddd
Jeffrey Frelick <jeff@autostructure.io>
2018-02-06 16:08:52 -0500

    check in to merge with master.

--- lib/puppet/provider/applockerpolicy/powershell.rb
+++ lib/puppet/provider/applockerpolicy/powershell.rb
@@ -4,7 +4,7 @@ Puppet::Type.type(:applockerpolicy).provide(:powershell) do
   desc 'Use the Windows O/S powershell.exe tool to manage AppLocker policies.'
   # windows only
   confine :kernel => :windows
-  #commands :ps => 'powershell.exe'
+  commands :ps => 'powershell.exe'
     # if File.exist?("#{ENV['SYSTEMROOT']}\\sysnative\\WindowsPowershell\\v1.0\\powershell.exe")
     #   "#{ENV['SYSTEMROOT']}\\sysnative\\WindowsPowershell\\v1.0\\powershell.exe"
     # elsif File.exist?("#{ENV['SYSTEMROOT']}\\system32\\WindowsPowershell\\v1.0\\powershell.exe")
