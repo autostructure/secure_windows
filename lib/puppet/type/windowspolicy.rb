0068bfc8bd1432e56884e9cbbe71e6fe02e724ce
Jeffrey Frelick <jeff@autostructure.io>
2018-02-18 17:05:30 -0500

    recode name param

--- lib/puppet/type/windowspolicy.rb
+++ lib/puppet/type/windowspolicy.rb
@@ -3,8 +3,20 @@ Puppet::Type.newtype(:windowspolicy) do
 
   ensurable
 
-  newparam(:name) do
-    isnamevar
+#  newparam(:name) do
+#    isnamevar
+#    puts 'windowspolicy.rb::name'
+#  end
+
+  newparam(:name, :namevar => true) do
+    desc "Interface name - currently must be 'friendly' name (e.g. Ethernet)"
     puts 'windowspolicy.rb::name'
+    munge do |value|
+      value.downcase
+    end
+    def insync?(is)
+      is.downcase == should.downcase
+    end
   end
+
 end
