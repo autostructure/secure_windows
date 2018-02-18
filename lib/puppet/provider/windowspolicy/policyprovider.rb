902307e98f24fc8c10487c113655d0fdef47a5f6
Jeffrey Frelick <jeff@autostructure.io>
2018-02-18 16:42:02 -0500

    testing with policyprovider

--- lib/puppet/provider/windowspolicy/policyprovider.rb
+++ lib/puppet/provider/windowspolicy/policyprovider.rb
@@ -12,14 +12,14 @@ Puppet::Type.type(:windowspolicy).provide(:policyprovider) do
 #    Puppet.debug xml_string
 #    xml_doc.root.each_element('RuleCollection') do |rc|
 #      rc.each_element('FilePathRule') do |fpr|
-        rule = {}
+        #rule = {}
 #        rule[:ensure]            = :present
 #        rule[:name]              = fpr.attribute('Name').to_string.slice(/=['|"]*(.*)['|"]/,1)
 #        rule[:description]       = fpr.attribute('Description').to_string.slice(/=['|"]*(.*)['|"]/,1)
-        rule = {:ensure => :present, :name => 'windowspolicytest'}
+        #rule = {:ensure => :present, :name => 'windowspolicytest'}
         puts 'instances0'
         #self.new(rule)
-        rule
+        #rule
         puts 'instances1'
 #      end
 #    end
