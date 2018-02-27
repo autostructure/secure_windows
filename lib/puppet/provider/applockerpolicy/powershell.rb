b62581bc55b1bb1b20d4671cebead1dcd68362d7
Jeffrey Frelick <jeff@autostructure.io>
2018-02-27 17:35:35 -0500

    debug powershell.set

--- lib/puppet/provider/applockerpolicy/powershell.rb
+++ lib/puppet/provider/applockerpolicy/powershell.rb
@@ -199,12 +199,12 @@ Puppet::Type.type(:applockerpolicy).provide(:powershell) do
         puts 'powershell.rb b4 delete_element'
         x = "//FilePathRule[@Id='#{@property_hash[:id]}']"
         puts x
-        puts 'parent='
-        puts x.parent.to_s
         e = xml_doc_should.get_elements x
         puts 'get_elements e='
         e.attributes['Description'] = 'TEST POWERSHELL SET' # = @property_hash['Description']
         puts e
+        puts 'e.parent='
+        puts e.parent
         puts 'xml_doc_should='
         puts xml_doc_should.root()
         xml_doc_should.delete_element e
