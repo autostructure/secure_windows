05e36a9e9729a2a85143f6e131a047b63e2b9527
Paul Talbot <paul@fullfrontalingenuity.com>
2018-03-06 21:26:39 -0500

    added windows_domaincontroller back

---
+++ lib/facter/windows_domaincontroller.rb
@@ -0,0 +1,9 @@
+Facter.add("windows_domaincontroller") do
+  confine operatingsystem: :windows
+  setcode do
+    powershell = 'C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe'
+    command = 'if ((Get-WindowsFeature -name adds).Installed){ write-host "true" } else { write-host "false" }'
+    #Facter::Util::Resolution.exec(%Q{#{powershell} -command "#{command}"})
+    Facter::Core::Execution.exec(%Q{#{powershell} -command "#{command}"})
+  end
+end
