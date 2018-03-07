61a1c5a3af0073c1b7dcaa0a3ff91ece1a39311c
Paul Talbot <paul@fullfrontalingenuity.com>
2018-03-06 20:49:01 -0500

    missing paren inside if

--- lib/facter/windows_domaincontroller.rb
+++ lib/facter/windows_domaincontroller.rb
@@ -2,7 +2,7 @@ Facter.add("windows_domaincontroller") do
   confine :osfamily => "windows"
   setcode do
     powershell = 'C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe'
-    command = 'if (Get-WindowsFeature -name adds).Installed){ write-host "true"; exit 0 } else { write-host "false"; exit 1 }'
+    command = 'if ((Get-WindowsFeature -name adds).Installed){ write-host "true"; exit 0 } else { write-host "false"; exit 1 }'
     Facter::Util::Resolution.exec(%Q{#{powershell} -command "#{command}"})
   end
 end
