780d910258d540fb69264e269526bf1fff41f149
Paul Talbot <paul@fullfrontalingenuity.com>
2018-03-06 21:09:17 -0500

    trying Execution.exec instead of Resolution

--- lib/facter/windows_domaincontroller.rb
+++ lib/facter/windows_domaincontroller.rb
@@ -3,6 +3,7 @@ Facter.add("windows_domaincontroller") do
   setcode do
     powershell = 'C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe'
     command = 'if ((Get-WindowsFeature -name adds).Installed){ write-host "true" } else { write-host "false" }'
-    Facter::Util::Resolution.exec(%Q{#{powershell} -command "#{command}"})
+    #Facter::Util::Resolution.exec(%Q{#{powershell} -command "#{command}"})
+    Facter::Core::Execution.exec(%Q{#{powershell} -command "#{command}"})
   end
 end
