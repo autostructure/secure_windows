ef16c2a25300e8355e7cb50fcc3d322cc19d7de5
Jeffrey Frelick <jeff@autostructure.io>
2018-03-08 09:00:01 -0500

    Fixing database_log_files

--- lib/facter/database_log_files.rb
+++ lib/facter/database_log_files.rb
@@ -2,9 +2,14 @@ Facter.add('database_log_files') do
   confine kernel: 'windows'
 
   setcode do
-    database_log_files_dir = Facter.value(:ntds_parameters)['DSA Working Directory']
+    database_log_files_dir_array = []
 
-    database_log_files_dir_array = Dir["#{database_log_files_dir}/*"]
+    begin
+      database_log_files_dir = Facter.value(:ntds_parameters)['DSA Working Directory']
+      database_log_files_dir_array = Dir["#{database_log_files_dir}/*"]
+    rescue e
+      Puppet.debug "database_log_files.rb: Error = #{e}"
+    end
 
     database_log_files_dir_array
   end
