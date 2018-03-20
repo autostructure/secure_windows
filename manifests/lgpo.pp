30612b9836829163119293835717e82caf0185a9
Jack Coleman <jack@autostructure.io>
2018-03-20 15:57:25 -0400

    a

--- manifests/lgpo.pp
+++ manifests/lgpo.pp
@@ -26,14 +26,14 @@ class secure_windows::lgpo {
     policy_value   => '3',
   }
 
-  # # V-73313
-  # # The period of time before the bad logon counter is reset must be configured to 15 minutes or greater.
-  # local_security_policy { 'Reset account lockout counter after':
-  #   ensure         => 'present',
-  #   policy_setting => 'ResetLockoutCount',
-  #   policy_type    => 'System Access',
-  #   policy_value   => '15',
-  # }
+  # V-73313
+  # The period of time before the bad logon counter is reset must be configured to 15 minutes or greater.
+  local_security_policy { 'Reset account lockout counter after':
+    ensure         => 'present',
+    policy_setting => 'ResetLockoutCount',
+    policy_type    => 'System Access',
+    policy_value   => '15',
+  }
   #
   # # V-73315
   # # The password history must be configured to 24 passwords remembered.
