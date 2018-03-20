923a0c3007628c7d65912f4a2a0b20902de144d4
Jack Coleman <jack@autostructure.io>
2018-03-20 15:29:20 -0400

    add features back

--- manifests/features.pp
+++ manifests/features.pp
@@ -9,13 +9,13 @@
 class secure_windows::features {
   $features_to_remove = [
     'fax',
-    # 'web-ftp-service',
-    # 'pnrp',
-    # 'simple-tcpip',
-    # 'telnet-client',
-    # 'tftp-client',
-    # 'fs-smb1',
-    # 'powershell-v2',
+    'web-ftp-service',
+    'pnrp',
+    'simple-tcpip',
+    'telnet-client',
+    'tftp-client',
+    'fs-smb1',
+    'powershell-v2',
   ]
 
   windowsfeature { $features_to_remove:
