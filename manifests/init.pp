b8ec215a2af81c1b12b63033cba0da90f8783344
Jack Coleman <jack@autostructure.io>
2018-03-20 13:19:52 -0400

    lgpo again

--- manifests/init.pp
+++ manifests/init.pp
@@ -14,6 +14,6 @@ class secure_windows {
   include ::secure_windows::auditpol
   include ::secure_windows::features
   include ::secure_windows::registry_editor
-  #include ::secure_windows::lgpo
+  include ::secure_windows::lgpo
 
 }
