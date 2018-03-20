a5ed4955f0dad24bb90e3029966fb897199f83fb
Jack Coleman <jack@autostructure.io>
2018-03-20 11:37:45 -0400

    merge

--- spec/classes/auditpol_spec.rb
+++ spec/classes/auditpol_spec.rb
@@ -2,48 +2,14 @@ require 'spec_helper'
 
 describe 'secure_windows::auditpol' do
   context 'auditpol defaults' do
-<<<<<<< HEAD
-    let(:facts) do
-      facts.merge(
-        windows_type: '2',
-        windows_server_type: 'windowsdc',
-        windows_role: '2',
-      )
-    end
-    it {
-      should contain_auditpol('Credential Validation').with(
-=======
     let(:facts) { { 'windows_type' => '2', 'operatingsystem' => 'windows' } }
 
     it {
       is_expected.to contain_auditpol('Credential Validation').with(
->>>>>>> 1f54c62fd1209b241fadda8809348850ba6c1918
         'success' => 'enable',
         'failure' => 'enable',
       )
     }
-<<<<<<< HEAD
-    it {
-      should contain_auditpol('Application Group Management').with(
-        'success' => 'enable',
-        'failure' => 'enable'
-      )
-    }
-    it {
-      should contain_auditpol('Computer Account Management').with(
-        'success' => 'enable',
-        'failure' => 'enable'
-      )
-    }
-    # DC ONLY
-    # it {
-    #   should contain_auditpol('Distribution Group Management').with(
-    #     'success' => 'enable',
-    #     'failure' => 'enable'
-    #   )
-    # }
-=======
->>>>>>> 1f54c62fd1209b241fadda8809348850ba6c1918
     it {
       is_expected.to contain_auditpol('Other Account Management Events').with(
         'success' => 'enable',
@@ -63,9 +29,6 @@ describe 'secure_windows::auditpol' do
       )
     }
     it {
-<<<<<<< HEAD
-      should contain_auditpol('Process Creation').with(
-=======
       is_expected.to contain_auditpol('Plug and Play Events').with(
         'success' => 'enable',
         'failure' => 'disable',
@@ -73,33 +36,13 @@ describe 'secure_windows::auditpol' do
     }
     it {
       is_expected.to contain_auditpol('Process Creation').with(
->>>>>>> 1f54c62fd1209b241fadda8809348850ba6c1918
         'success' => 'enable',
         'failure' => 'disable',
       )
     }
-<<<<<<< HEAD
-    # DC ONLY
-    # it {
-    #   should contain_auditpol('Directory Service Changes').with(
-    #     'success' => 'enable',
-    #     'failure' => 'enable'
-    #   )
-    # }
-    # it {
-    #   should contain_auditpol('Directory Service Access').with(
-    #     'success' => 'enable',
-    #     'failure' => 'enable'
-    #   )
-    # }
-=======
->>>>>>> 1f54c62fd1209b241fadda8809348850ba6c1918
     it {
       is_expected.to contain_auditpol('Account Lockout').with(
         'success' => 'enable',
-<<<<<<< HEAD
-        'failure' => 'disable'
-=======
         'failure' => 'enable',
       )
     }
@@ -107,7 +50,6 @@ describe 'secure_windows::auditpol' do
       is_expected.to contain_auditpol('Group Membership').with(
         'success' => 'enable',
         'failure' => 'disable',
->>>>>>> 1f54c62fd1209b241fadda8809348850ba6c1918
       )
     }
     it {
@@ -123,17 +65,6 @@ describe 'secure_windows::auditpol' do
       )
     }
     it {
-<<<<<<< HEAD
-      should contain_auditpol('Other Logon/Logoff Events').with(
-        'success' => 'enable',
-        'failure' => 'enable'
-      )
-    }
-    it {
-      should contain_auditpol('Special Logon').with(
-        'success' => 'enable',
-        'failure' => 'disable'
-=======
       is_expected.to contain_auditpol('Special Logon').with(
         'success' => 'enable',
         'failure' => 'disable',
@@ -143,7 +74,6 @@ describe 'secure_windows::auditpol' do
       is_expected.to contain_auditpol('Removable Storage').with(
         'success' => 'enable',
         'failure' => 'enable',
->>>>>>> 1f54c62fd1209b241fadda8809348850ba6c1918
       )
     }
     it {
@@ -159,9 +89,6 @@ describe 'secure_windows::auditpol' do
       )
     }
     it {
-<<<<<<< HEAD
-      should contain_auditpol('Sensitive Privilege Use').with(
-=======
       is_expected.to contain_auditpol('Authorization Policy Change').with(
         'success' => 'enable',
         'failure' => 'disable',
@@ -169,7 +96,6 @@ describe 'secure_windows::auditpol' do
     }
     it {
       is_expected.to contain_auditpol('Sensitive Privilege Use').with(
->>>>>>> 1f54c62fd1209b241fadda8809348850ba6c1918
         'success' => 'enable',
         'failure' => 'enable',
       )
@@ -204,8 +130,6 @@ describe 'secure_windows::auditpol' do
         'failure' => 'enable',
       )
     }
-<<<<<<< HEAD
-=======
   end
   context 'auditpol domain controller' do
     let(:facts) { { 'windows_type' => '2', 'operatingsystem' => 'windows', 'windows_server_type' => 'windowsdc' } }
@@ -228,6 +152,5 @@ describe 'secure_windows::auditpol' do
         'failure' => 'enable',
       )
     }
->>>>>>> 1f54c62fd1209b241fadda8809348850ba6c1918
   end
 end
