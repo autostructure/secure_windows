3071ee38e11979ce08d09d0de81bbdac81ad4125
Paul Talbot <paul@fullfrontalingenuity.com>
2018-03-20 11:56:27 -0400

    breaking change - added structure for proposed organization of STIGs and associated in-module hiera, classes/files will need to correct placeholder for OS version in init.pp and move profile stuff into classes

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,7 +7,11 @@
 # - GPO
 # - OTHER
 #
-class secure_windows {
+
+class secure_windows (
+  [Optional]Boolean $is_dod,
+  [Optional]String $classification,
+) {
 
   include ::secure_windows::acl
   include ::secure_windows::ad_acl
@@ -16,4 +20,41 @@ class secure_windows {
   include ::secure_windows::registry_editor
   # include ::secure_windows::lgpo
 
-}
+  # Platform-specific defaults
+  case $fact['whatever_osfullversionstring_is'] {
+
+    'Windows Server 2012','Windows Server 2012 R2': {
+      class { 'secure_windows::STIG::v73605':
+        is_dod         => $is_dod,
+        classification => $classification,
+      }
+      class { 'secure_windows::STIG::v73607':
+        is_dod         => $is_dod,
+        classification => $classification,
+      }
+      class { 'secure_windows::STIG::v73609':
+        is_dod         => $is_dod,
+        classification => $classification,
+      }
+    }
+
+    'Windows Server 2016','Windows Server 2016 R2': {
+      class { 'secure_windows::STIG::v73605':
+        is_dod         => $is_dod,
+        classification => $classification,
+      }
+      class { 'secure_windows::STIG::v73607':
+        is_dod         => $is_dod,
+        classification => $classification,
+      }
+      class { 'secure_windows::STIG::v73609':
+        is_dod         => $is_dod,
+        classification => $classification,
+      }
+    }
+
+    default: {
+      fail("Unsupported operating system ($fact['whatever_osfullversionstring_is']) detected.")
+    }
+
+  }
