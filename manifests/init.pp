8244e7153c7250e28f95ce6912ad06428ce1e9c0
Jack Coleman <jack@autostructure.io>
2018-03-20 11:43:00 -0400

    remove ad_acl

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,7 +10,7 @@
 class secure_windows {
 
   #include ::secure_windows::acl
-  include ::secure_windows::ad_acl
+  #include ::secure_windows::ad_acl
   include ::secure_windows::auditpol
   include ::secure_windows::features
   include ::secure_windows::registry_editor
