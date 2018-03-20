cffed45b68caabd743a93f6a814a7d85c9a72d8a
Jack Coleman <jack@autostructure.io>
2018-03-20 13:06:52 -0400

    just auditpol

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,8 +12,8 @@ class secure_windows {
   #include ::secure_windows::acl
   #include ::secure_windows::ad_acl
   include ::secure_windows::auditpol
-  include ::secure_windows::features
-  include ::secure_windows::registry_editor
-  include ::secure_windows::lgpo
+  #include ::secure_windows::features
+  #include ::secure_windows::registry_editor
+  #include ::secure_windows::lgpo
 
 }
