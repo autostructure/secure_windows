daa9bf852f754c7e26670d4b1493a67b183e0759
Jack Coleman <jack@autostructure.io>
2018-03-21 17:40:07 -0400

    new file

---
+++ manifests/gpo.pp
@@ -0,0 +1,19 @@
+# This class manages GPOs
+class secure_windows::gpo {
+
+  # V-73359
+  # Kerberos user logon restrictions must be enforced.
+
+
+  # V-73361
+  # The Kerberos service ticket maximum lifetime must be limited to 600 minutes or less.
+
+
+  # V-73363
+  # The Kerberos user ticket lifetime must be limited to 10 hours or less.
+
+
+  # V-73365
+  # The Kerberos policy user ticket renewal maximum lifetime must be limited to seven days or less.
+
+}
