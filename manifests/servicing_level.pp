78e7fb32bdb9a7c112d0052711106118e2616e56
Jack Coleman <jack@autostructure.io>
2018-03-27 14:43:05 -0400

    Add servicing_level requirement

--- manifests/servicing_level.pp
+++ manifests/servicing_level.pp
@@ -1,8 +1,16 @@
 # This class manages the servicing level requirement
 class secure_windows::servicing_level {
-  if($facts['kernelversion'] >= 10.0.14393) {
-    notify { 'message':
-      message => 'hello',
+
+  # V-73239
+  # Systems must be maintained at a supported servicing level.
+  if versioncmp($facts['kernel_version'], '10.0.14393') >= 0 {
+    notify { 'In compliance with V-73239':
+      message => 'In compliance with V-73239',
+    }
+  }
+  else {
+    notify { 'Not in compliance with V-73239':
+      message => 'Not in compliance with V-73239. Please update the system to a supported servicing level.',
     }
   }
 }
