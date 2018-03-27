0f3a7829de130d8b1837f347605f783362bf9820
Jack Coleman <jack@autostructure.io>
2018-03-27 14:54:03 -0400

    edit message

--- manifests/servicing_level.pp
+++ manifests/servicing_level.pp
@@ -5,12 +5,12 @@ class secure_windows::servicing_level {
   # Systems must be maintained at a supported servicing level.
   if versioncmp($facts['kernelversion'], '10.0.14394') >= 0 {
     notify { 'In compliance with V-73239':
-      message => 'In compliance with V-73239. System is at a supported servicing level.',
+      message => 'In compliance with DoD STIG V-73239. System is at a supported servicing level.',
     }
   }
   else {
     notify { 'Not in compliance with V-73239':
-      message  => 'Not in compliance with V-73239. Please update the system to a supported servicing level.',
+      message  => 'Not in compliance with DoD STIG V-73239. Please update the system to a supported servicing level.',
       loglevel => warning,
     }
   }
