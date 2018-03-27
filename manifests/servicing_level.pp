12f0faf9f7defd3816475c60834c1f93f41cd450
Jack Coleman <jack@autostructure.io>
2018-03-27 14:46:46 -0400

    test a failure

--- manifests/servicing_level.pp
+++ manifests/servicing_level.pp
@@ -3,14 +3,15 @@ class secure_windows::servicing_level {
 
   # V-73239
   # Systems must be maintained at a supported servicing level.
-  if versioncmp($facts['kernelversion'], '10.0.14393') >= 0 {
+  if versioncmp($facts['kernelversion'], '10.0.14394') >= 0 {
     notify { 'In compliance with V-73239':
-      message => 'In compliance with V-73239',
+      message => 'In compliance with V-73239. System is at a supported servicing level.',
     }
   }
   else {
     notify { 'Not in compliance with V-73239':
-      message => 'Not in compliance with V-73239. Please update the system to a supported servicing level.',
+      message  => 'Not in compliance with V-73239. Please update the system to a supported servicing level.',
+      loglevel => fail,
     }
   }
 }
