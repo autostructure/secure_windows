6ef73e17613e431331c5d2cdb049debb8fc6d4d3
Jack Coleman <jack@autostructure.io>
2018-03-29 10:53:12 -0400

    final code for ntp

--- manifests/ntp.pp
+++ manifests/ntp.pp
@@ -8,16 +8,36 @@ class secure_windows::ntp {
 
   if $facts['windows_type'] =~ /(1|3|4|5)/ {
     # Domain-Joined Systems
-    registry::value { 'v73307 Type':
-      key    => 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\W32time\Parameters',
-      value  => 'Type',
-      type   => 'string',
-      data   => 'NT5DS',
-      notify => Service['w32time'],
+    if $facts['fqdn'] == $facts['pdc_emulator_role'] {
+      # PDC Emulator Role should sync with a DoD time source
+      registry::value { 'v73307 Type':
+        key    => 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\W32time\Parameters',
+        value  => 'Type',
+        type   => 'string',
+        data   => 'NTP',
+        notify => Service['w32time'],
+      }
+      registry::value { 'v73307 NtpServer':
+        key    => 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\W32time\Parameters',
+        value  => 'NtpServer',
+        type   => 'string',
+        data   => 'tick.usno.navy.mil',
+        notify => Service['w32time'],
+      }
+    }
+    else {
+      # Domain Members, not the PDC, should sync with the PDC using NT5DS
+      registry::value { 'v73307 Type':
+        key    => 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\W32time\Parameters',
+        value  => 'Type',
+        type   => 'string',
+        data   => 'NT5DS',
+        notify => Service['w32time'],
+      }
     }
   }
   else {
-    # Standalone Servers
+    # Standalone Servers should sync with a DoD time source
     registry::value { 'v73307 Type':
       key    => 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\W32time\Parameters',
       value  => 'Type',
