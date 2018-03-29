00d3f95c60879d90eeb17a1d0fa872e5bfde4cb4
Jack Coleman <jack@autostructure.io>
2018-03-29 10:46:19 -0400

    style change

--- lib/facter/pdc_emulator_role.rb
+++ lib/facter/pdc_emulator_role.rb
@@ -5,7 +5,7 @@ Facter.add('pdc_emulator_role') do
   confine operatingsystem: :windows
   setcode do
     powershell = 'C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe'
-    command = '(get-addomain).pdcemulator'
+    command = '(Get-ADDomain).PDCEmulator'
     Facter::Core::Execution.exec(%(#{powershell} -command "#{command}"))
   end
 end
