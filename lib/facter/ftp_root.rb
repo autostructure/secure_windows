dbad770a8d2aea36e4ce90f4efa86e77b03b0515
Jack Coleman <jack@autostructure.io>
2018-04-02 18:24:43 -0400

    change the way we call the command?

--- lib/facter/ftp_root.rb
+++ lib/facter/ftp_root.rb
@@ -5,6 +5,6 @@ Facter.add('ftp_root') do
   setcode do
     powershell = 'C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe'
     command = "[Void][Reflection.Assembly]::LoadWithPartialName(\"Microsoft.Web.Administration\"); write ((((New-Object Microsoft.Web.Administration.ServerManager).Sites | where { $_.Name -eq \"#{Facter.value(:fqdntest)}\" }).Applications | where { $_.Path -eq \"/\" }).VirtualDirectories | where { $_.Path -eq \"/\" }).PhysicalPath"
-    Facter::Core::Execution.exec(%(#{powershell} -command "#{command}"))
+    Facter::Core::Execution.exec("#{powershell} -command \"#{command}\"")
   end
 end
