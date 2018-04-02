2ae7b38f03c1eacedfc7a59a00275bf0554ccc04
Jack Coleman <jack@autostructure.io>
2018-04-02 17:53:40 -0400

    using test fact since my server doesn't have a real fqdn yet

--- lib/facter/ftp_root.rb
+++ lib/facter/ftp_root.rb
@@ -4,7 +4,7 @@ Facter.add('ftp_root') do
   confine operatingsystem: :windows
   setcode do
     powershell = 'C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe'
-    command = "[Void][Reflection.Assembly]::LoadWithPartialName(\"Microsoft.Web.Administration\"); write ((((New-Object Microsoft.Web.Administration.ServerManager).Sites | where { $_.Name -eq #{Facter.value(:fqdn)} }).Applications | where { $_.Path -eq \"/\" }).VirtualDirectories | where { $_.Path -eq \"/\" }).PhysicalPath"
+    command = "[Void][Reflection.Assembly]::LoadWithPartialName(\"Microsoft.Web.Administration\"); write ((((New-Object Microsoft.Web.Administration.ServerManager).Sites | where { $_.Name -eq #{Facter.value(:fqdntest)} }).Applications | where { $_.Path -eq \"/\" }).VirtualDirectories | where { $_.Path -eq \"/\" }).PhysicalPath"
     Facter::Core::Execution.exec(%(#{powershell} -command "#{command}"))
   end
 end
