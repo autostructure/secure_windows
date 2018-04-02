18def23a44305bd38e55e9c1854b77374024c4ae
Jack Coleman <jack@autostructure.io>
2018-04-02 17:41:33 -0400

    inital commit for 303 and 305 logic

---
+++ lib/facter/ftp_root.rb
@@ -0,0 +1,10 @@
+# This fact returns the ftp root of a FTP Server with a site equal to fqdn
+require 'facter'
+Facter.add('ftp_root') do
+  confine operatingsystem: :windows
+  setcode do
+    powershell = 'C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe'
+    command = "write ((((New-Object Microsoft.Web.Administration.ServerManager).Sites | where { $_.Name -eq #{Facter.value(:fqdn)} }).Applications | where { $_.Path -eq "/" }).VirtualDirectories | where { $_.Path -eq "/" }).PhysicalPath"
+    Facter::Core::Execution.exec(%(#{powershell} -command "#{command}"))
+  end
+end
--- manifests/ftp_servers.pp
+++ manifests/ftp_servers.pp
@@ -1,12 +1,41 @@
 # This class manages settings for FTP servers
 class secure_windows::ftp_servers {
+
+  # The fact windows_role returns a comma separated list of role IDs
+  # The role ID for FTP Server is 184. We are checking if 184 is in the list
+  #   so that these resources only apply to FTP Servers
   if ($facts['windows_role'] and
       $facts['windows_role'] =~ /(^184|,184,|,184$)/) {
+
+    # Test to make sure this works on FTP Servers. Remove for Production
     notify { 'FTP Server':
       message => 'I am a FTP Server!',
     }
+
+    # V-73303
+    # FTP servers must be configured to prevent anonymous logons.
+    #
+    # NOTE: - This command does not have a corresponding 'get' command. I would have to parse an XML file.
+    #         For now, I will leave it to run this command every time since it is idempotent.
+    #       - This only works for servers who's FTP server site is the same as the FQDN. A server could
+    #         create an IIS FTP Server at a different address and we would not know about it.
+    exec { 'Set FTP anynymousAuthentication to Disabled':
+      command => "%systemroot%\\system32\\inetsrv\\AppCmd.exe set config -section:system.applicationHost/sites /[name='${facts['fqdn']}'].ftpServer.security.authentication.anonymousAuthentication.enabled:"False" /commit:apphost",
+    }
+
+    # V-73305
+    # FTP servers must be configured to prevent access to the system drive.
+    if $facts['ftp_root'] ~= /(C:\$|C:\Program Files|C:\Windows)/ {
+      notify { 'Not in compliance with DoD STIG V-73305':
+        message  => 'Not in compliance with DoD STIG V-73305. The FTP Server\'s root contains the system files.',
+        loglevel => warning,
+      }
+    }
   }
 }
+#
+# ensure $facts['ftp_root'] != 'C:\ProgramData\ | C:\Program Files\ | C:\Windows'
+#
 #
 # %systemroot%\system32\inetsrv\AppCmd.exe set config -section:system.applicationHost/sites /[name='win08.example.com'].ftpServer.security.authentication.anonymousAuthentication.enabled:"False" /commit:apphost
 #
