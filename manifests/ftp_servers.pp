da79c4246ea7405e4a3ce6671b152b06d07af83e
Jack Coleman <jack@autostructure.io>
2018-04-03 11:21:12 -0400

    testing for non-compliant ftp root

--- manifests/ftp_servers.pp
+++ manifests/ftp_servers.pp
@@ -8,15 +8,16 @@ class secure_windows::ftp_servers {
       $facts['windows_role'] =~ /(^184|,184,|,184$)/) {
 
     # Test to make sure this works on FTP Servers. Remove for Production
-    notify { 'FTP Server':
-      message => 'I am a FTP Server!',
-    }
+    # notify { 'FTP Server':
+    #   message => 'I am a FTP Server!',
+    # }
 
     # V-73303
     # FTP servers must be configured to prevent anonymous logons.
     #
     # NOTE: - This command does not have a corresponding 'get' command. I would have to parse an XML file.
     #         For now, I will leave it to run this command every time since it is idempotent.
+    #       - Really should make this idempotent so it doesn't show an intentional change every 30 min
     #       - This only works for servers who's FTP server site is the same as the FQDN. A server could
     #         create an IIS FTP Server at a different address and we would not know about it.
     exec { 'Set FTP anynymousAuthentication to Disabled':
@@ -25,7 +26,7 @@ class secure_windows::ftp_servers {
 
     # V-73305
     # FTP servers must be configured to prevent access to the system drive.
-    if $facts['ftp_root'] =~ /(C:\$|C:\Program Files|C:\Windows)/ {
+    if $facts['ftp_root'] =~ /(C:\$|C:\Program Files|C:\Windows|C:\inetpub\ftproot)/ {
       notify { 'Not in compliance with DoD STIG V-73305':
         message  => 'Not in compliance with DoD STIG V-73305. The FTP Server\'s root contains the system files.',
         loglevel => warning,
