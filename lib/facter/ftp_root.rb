708d786a17fb7b1bc9318dd19ec11595cc9ab6ec
Jack Coleman <jack@autostructure.io>
2018-04-03 10:52:29 -0400

    take out first part that's getting errors

--- lib/facter/ftp_root.rb
+++ lib/facter/ftp_root.rb
@@ -4,7 +4,8 @@ Facter.add('ftp_root') do
   confine operatingsystem: :windows
   setcode do
     powershell = 'C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe'
-    command = "[Void][Reflection.Assembly]::LoadWithPartialName(\"Microsoft.Web.Administration\"); write ((((New-Object Microsoft.Web.Administration.ServerManager).Sites | where { $_.Name -eq '#{Facter.value(:fqdntest)}' }).Applications | where { $_.Path -eq '/' }).VirtualDirectories | where { $_.Path -eq '/' }).PhysicalPath"
+    #[Void][Reflection.Assembly]::LoadWithPartialName(\"Microsoft.Web.Administration\"); 
+    command = "write ((((New-Object Microsoft.Web.Administration.ServerManager).Sites | where { $_.Name -eq '#{Facter.value(:fqdntest)}' }).Applications | where { $_.Path -eq '/' }).VirtualDirectories | where { $_.Path -eq '/' }).PhysicalPath"
     Facter::Core::Execution.execute("#{powershell} -command \"#{command}\"")
   end
 end
