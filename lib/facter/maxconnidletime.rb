0b92c081fa477cf8a3be906c2489a9a1b956f64c
Jack Coleman <jack@autostructure.io>
2018-04-03 14:34:31 -0400

    try this

--- lib/facter/maxconnidletime.rb
+++ lib/facter/maxconnidletime.rb
@@ -5,7 +5,7 @@ Facter.add('maxconnidletime') do
   setcode do
     powershell = 'C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe'
     command = "write ((Get-ADObject -SearchBase \"CN=Query-Policies,CN=Directory Service,CN=Windows NT,CN=Services,CN=Configuration,DC=example,DC=com\" -Filter 'ObjectClass -like \"queryPolicy\"' -Properties ldapadminlimits).ldapadminlimits -like \"MaxConnIdleTime*\")"
-    $time = Facter::Core::Execution.execute("#{powershell} -command \"#{command}\"")
+    $time = Facter::Core::Execution.execute("#{powershell} -command (#{command})")
     $time[/^MaxConnIdleTime=(.*)/]
   end
 end
