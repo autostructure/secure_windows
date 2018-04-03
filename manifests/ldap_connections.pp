90b10fa03e4d46e51f79a3cc3892bd184afbd16c
Jack Coleman <jack@autostructure.io>
2018-04-03 15:25:46 -0400

    convert fact from string to integer for comparison

--- manifests/ldap_connections.pp
+++ manifests/ldap_connections.pp
@@ -3,6 +3,7 @@ class secure_windows::ldap_connections {
 
   # V-73387
   # The directory service must be configured to terminate LDAP-based network connections to the directory server after 5 minutes of inactivity.
+  $num = 0 + $facts['maxconnidletime']
   if $facts['maxconnidletime'] > 300 {
     exec { 'V-73387: Set NTDSUtil MaxConnIdleTime':
       command => "C:\\Windows\\System32\\ntdsutil.exe \"LDAP policies\" \"connections\" \"connect to server ${facts['fqdn']}\" \"q\" \"Set MaxConnIdleTime to 300\" \"Commit Changes\" \"q\" \"q\""
