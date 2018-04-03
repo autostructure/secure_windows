98a60f5eb08e63eba48f4d04b1cddfe274949e7a
Jack Coleman <jack@autostructure.io>
2018-04-03 11:42:09 -0400

    Cleanup comments and remove test values

--- lib/facter/ftp_root.rb
+++ lib/facter/ftp_root.rb
@@ -4,9 +4,7 @@ Facter.add('ftp_root') do
   confine operatingsystem: :windows
   setcode do
     powershell = 'C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe'
-    #[Void][Reflection.Assembly]::LoadWithPartialName(\"Microsoft.Web.Administration\");
-    command = "[Void][Reflection.Assembly]::LoadWithPartialName('Microsoft.Web.Administration'); write ((((New-Object Microsoft.Web.Administration.ServerManager).Sites | where { $_.Name -eq '#{Facter.value(:fqdntest)}' }).Applications | where { $_.Path -eq '/' }).VirtualDirectories | where { $_.Path -eq '/' }).PhysicalPath"
-    #Facter::Core::Execution.execute("#{powershell} -command \"[Void][Reflection.Assembly]::LoadWithPartialName(\"\"Microsoft.Web.Administration\"\")\"")
+    command = "[Void][Reflection.Assembly]::LoadWithPartialName('Microsoft.Web.Administration'); write ((((New-Object Microsoft.Web.Administration.ServerManager).Sites | where { $_.Name -eq '#{Facter.value(:fqdn)}' }).Applications | where { $_.Path -eq '/' }).VirtualDirectories | where { $_.Path -eq '/' }).PhysicalPath"
     Facter::Core::Execution.execute("#{powershell} -command \"#{command}\"")
   end
 end
