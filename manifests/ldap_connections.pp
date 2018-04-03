ae7445c082724e3d8db79a283ef3e3540ba4de6d
Jack Coleman <jack@autostructure.io>
2018-04-03 15:11:28 -0400

    test

--- manifests/ldap_connections.pp
+++ manifests/ldap_connections.pp
@@ -3,7 +3,7 @@ class secure_windows::ldap_connections {
 
   # V-73387
   # The directory service must be configured to terminate LDAP-based network connections to the directory server after 5 minutes of inactivity.
-  exec { 'V-73387: Set NTDSUtil MaxConnIdleTime':
-    command => 'C:\\Windows\\System32\\ntdsutil.exe "LDAP policies" "connections" "connect to server win10.example.com" "q" "Set MaxConnIdleTime to 450" "Commit Changes" "q" "q"'
-  }
+  # exec { 'V-73387: Set NTDSUtil MaxConnIdleTime':
+  #   command => 'C:\\Windows\\System32\\ntdsutil.exe "LDAP policies" "connections" "connect to server win10.example.com" "q" "Set MaxConnIdleTime to 450" "Commit Changes" "q" "q"'
+  # }
 }
