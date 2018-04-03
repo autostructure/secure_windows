51cc649a12b36a3fcee7add29d813c043abb9465
Jack Coleman <jack@autostructure.io>
2018-04-03 15:17:08 -0400

    test

--- lib/facter/maxconnidletime.rb
+++ lib/facter/maxconnidletime.rb
@@ -4,6 +4,6 @@ Facter.add('maxconnidletime') do
   confine operatingsystem: :windows
   setcode do
     $time = Facter::Core::Execution.execute("dsquery * \"cn=Default Query Policy,cn=Query-Policies,cn=Directory Service,cn=Windows NT,cn=Services,cn=Configuration,dc=example,dc=com\" -attr LDAPAdminLimits")
-    $time[/^MaxConnIdleTime=(.*)/]
+    $time.match(/MaxConnIdleTime=(\d*)/)[1]
   end
 end
