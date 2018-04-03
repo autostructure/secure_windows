8b2d88d0a97f90962bae4ffe2262aa352722928d
Jack Coleman <jack@autostructure.io>
2018-04-03 15:31:10 -0400

    Only applies to DCs

--- manifests/ldap_connections.pp
+++ manifests/ldap_connections.pp
@@ -1,12 +1,13 @@
 # This class manages V-73387
 class secure_windows::ldap_connections {
-
-  # V-73387
-  # The directory service must be configured to terminate LDAP-based network connections to the directory server after 5 minutes of inactivity.
-  $num = 0 + $facts['maxconnidletime']
-  if $num > 300 {
-    exec { 'V-73387: Set NTDSUtil MaxConnIdleTime':
-      command => "C:\\Windows\\System32\\ntdsutil.exe \"LDAP policies\" \"connections\" \"connect to server ${facts['fqdn']}\" \"q\" \"Set MaxConnIdleTime to 300\" \"Commit Changes\" \"q\" \"q\""
+  if($facts['windows_server_type'] == 'windowsdc') {
+    # V-73387
+    # The directory service must be configured to terminate LDAP-based network connections to the directory server after 5 minutes of inactivity.
+    $num = 0 + $facts['maxconnidletime']
+    if $num > 300 {
+      exec { 'V-73387: Set NTDSUtil MaxConnIdleTime':
+        command => "C:\\Windows\\System32\\ntdsutil.exe \"LDAP policies\" \"connections\" \"connect to server ${facts['fqdn']}\" \"q\" \"Set MaxConnIdleTime to 300\" \"Commit Changes\" \"q\" \"q\""
+      }
     }
   }
 }
