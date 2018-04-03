ef3123fe60f5df261952339f112c9b1c006c89ee
Jack Coleman <jack@autostructure.io>
2018-04-03 11:08:21 -0400

    try single quotes?

--- lib/facter/ftp_root.rb
+++ lib/facter/ftp_root.rb
@@ -5,8 +5,8 @@ Facter.add('ftp_root') do
   setcode do
     powershell = 'C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe'
     #[Void][Reflection.Assembly]::LoadWithPartialName(\"Microsoft.Web.Administration\");
-    command = "write ((((New-Object Microsoft.Web.Administration.ServerManager).Sites | where { $_.Name -eq '#{Facter.value(:fqdntest)}' }).Applications | where { $_.Path -eq '/' }).VirtualDirectories | where { $_.Path -eq '/' }).PhysicalPath"
-    Facter::Core::Execution.execute("#{powershell} -command \"[Void][Reflection.Assembly]::LoadWithPartialName(\"\"Microsoft.Web.Administration\"\")\"")
+    command = "[Void][Reflection.Assembly]::LoadWithPartialName('Microsoft.Web.Administration'); write ((((New-Object Microsoft.Web.Administration.ServerManager).Sites | where { $_.Name -eq '#{Facter.value(:fqdntest)}' }).Applications | where { $_.Path -eq '/' }).VirtualDirectories | where { $_.Path -eq '/' }).PhysicalPath"
+    #Facter::Core::Execution.execute("#{powershell} -command \"[Void][Reflection.Assembly]::LoadWithPartialName(\"\"Microsoft.Web.Administration\"\")\"")
     Facter::Core::Execution.execute("#{powershell} -command \"#{command}\"")
   end
 end
