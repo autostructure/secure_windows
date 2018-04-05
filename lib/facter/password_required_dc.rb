0f5bd216a58a367d99a8d605546964e896a1057b
Jack Coleman <jack@autostructure.io>
2018-04-05 17:01:53 -0400

    waiting on a dc to test this

---
+++ lib/facter/password_required_dc.rb
@@ -0,0 +1,11 @@
+#
+# This fact returns true if AD accounts exist that do not require a password
+#
+Facter.add('password_required_dc') do
+  confine operatingsystem: :windows
+  setcode do
+    powershell = 'C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe'
+    command = 'Get-Aduser -Filter * -Properties Passwordnotrequired |FT Name, Passwordnotrequired, Enabled'
+    Facter::Core::Execution.exec(%(#{powershell} -command "#{command}"))
+  end
+end
--- manifests/stig/v73261.pp
+++ manifests/stig/v73261.pp
@@ -2,7 +2,12 @@
 # Accounts must require passwords.
 class secure_windows::stig::v73261 {
   if $facts['windows_server_type'] == 'windowsdc' {
-
+    if $facts['password_required_dc'] != '' {
+      notify { 'Not in compliance with DoD STIG V-73261':
+        message  => 'Not in compliance with DoD STIG V-73261. AD accounts exist that do not require passwords. Run \'net user [username] /passwordreq:yes\' for each finding.',
+        loglevel => warning,
+      }
+    }
   }
   else {
     if $facts['password_required'] != '' {
