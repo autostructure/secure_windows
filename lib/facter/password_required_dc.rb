351c00d79cf0f45c868e9a1707b5d5e26d9fd322
Jack Coleman <jack@autostructure.io>
2018-04-05 18:56:14 -0400

    test

--- lib/facter/password_required_dc.rb
+++ lib/facter/password_required_dc.rb
@@ -5,7 +5,7 @@ Facter.add('password_required_dc') do
   confine operatingsystem: :windows
   setcode do
     powershell = 'C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe'
-    command = 'Get-Aduser -Filter * -Properties Passwordnotrequired |FT Name, Passwordnotrequired, Enabled'
+    command = 'Get-Aduser -Filter * -Properties Passwordnotrequired | Where-Object { $_.Enabled -eq $true } | Where-Object { $_.Passwordnotrequired -eq $true } | FT Name, Passwordnotrequired, Enabled'
     Facter::Core::Execution.exec(%(#{powershell} -command "#{command}"))
   end
 end
