c030c11cf54beb1b46a73ba895a570d1becc6d94
Jack Coleman <jack@autostructure.io>
2018-04-05 18:30:15 -0400

    test

---
+++ manifests/stig/v73309.pp
@@ -0,0 +1,15 @@
+# V-73309
+# Windows 2016 account lockout duration must be configured to 15 minutes or greater.
+class secure_windows::stig::v73309 (
+  Boolean $enforced = false,
+) {
+  notify { 'message135':
+    message => 'enforced!',
+  }
+  local_security_policy { 'Account lockout duration':
+    ensure         => 'present',
+    policy_setting => 'LockoutDuration',
+    policy_type    => 'System Access',
+    policy_value   => '15',
+  }
+}
