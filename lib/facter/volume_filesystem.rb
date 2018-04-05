e508d40d33361273ebbe1c08171e176f1db92015
Jack Coleman <jack@autostructure.io>
2018-04-05 17:47:33 -0400

    test

--- lib/facter/volume_filesystem.rb
+++ lib/facter/volume_filesystem.rb
@@ -7,7 +7,13 @@ Facter.add('volume_filesystem') do
     powershell = 'C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe'
     command = 'Get-Volume | Select FileSystem | FT -HideTableHeaders'
     $filesystems = Facter::Core::Execution.exec(%(#{powershell} -command "#{command}"))
-    #$filesystems.split("\n")
+    $filesystems.split("\n")
+    $filesystems.each do |filesystem|
+      if filesystem.match(/(NTFS|ReFS)/)
+      else
+        return false
+      end
+    end
     #$filesystems.match(/MaxConnIdleTime=(\d*)/)[1]
   end
 end
