8038b3a3326c44caffe96a9ea53181c9e5999349
Jack Coleman <jack@autostructure.io>
2018-04-05 18:52:07 -0400

    test

--- lib/facter/password_expire_dc.rb
+++ lib/facter/password_expire_dc.rb
@@ -5,7 +5,7 @@ Facter.add('password_expire_dc') do
   confine operatingsystem: :windows
   setcode do
     powershell = 'C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe'
-    command = 'Search-ADAccount -PasswordNeverExpires -UsersOnly | FT Name, PasswordNeverExpires, Enabled'
+    command = 'Search-ADAccount -PasswordNeverExpires -UsersOnly | Where-Object { $_.Enabled -eq $true } | FT Name, PasswordNeverExpires, Enabled'
     Facter::Core::Execution.exec(%(#{powershell} -command "#{command}"))
   end
 end
