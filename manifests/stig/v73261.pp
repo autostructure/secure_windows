5429dd964f6f60af4dd929faa17165f54b1d8c5a
Jack Coleman <jack@autostructure.io>
2018-04-05 16:55:32 -0400

    test

--- manifests/stig/v73261.pp
+++ manifests/stig/v73261.pp
@@ -7,7 +7,7 @@ class secure_windows::stig::v73261 {
   else {
     if $facts['password_required'] != '' {
       notify { 'Not in compliance with DoD STIG V-73261':
-        message  => 'Not in compliance with DoD STIG V-73261. Local accounts exist that do not require passwords. Run net user [username] /passwordreq:yes for each finding.',
+        message  => 'Not in compliance with DoD STIG V-73261. Local accounts exist that do not require passwords. Run \'net user [username] /passwordreq:yes\' for each finding.',
         loglevel => warning,
       }
     }
