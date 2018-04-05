3d23260def6d1c5975d6132465a282bce3798491
Jack Coleman <jack@autostructure.io>
2018-04-05 17:42:05 -0400

    test

--- lib/facter/volume_filesystem.rb
+++ lib/facter/volume_filesystem.rb
@@ -5,9 +5,9 @@ Facter.add('volume_filesystem') do
   confine operatingsystem: :windows
   setcode do
     powershell = 'C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe'
-    command = 'Get-Volume | Select FileSystem'
+    command = 'Get-Volume | Select FileSystem | FT -HideTableHeaders'
     $filesystems = Facter::Core::Execution.exec(%(#{powershell} -command "#{command}"))
-    $filesystems.spit("\n")
+    #$filesystems.split("\n")
     #$filesystems.match(/MaxConnIdleTime=(\d*)/)[1]
   end
 end
