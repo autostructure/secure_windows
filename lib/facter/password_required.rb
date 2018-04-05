d061be180927cc4c1c0f5bcf7800c35dbb9bf344
Jack Coleman <jack@autostructure.io>
2018-04-05 16:53:57 -0400

    test

--- lib/facter/password_required.rb
+++ lib/facter/password_required.rb
@@ -5,7 +5,7 @@ Facter.add('password_required') do
   confine operatingsystem: :windows
   setcode do
     powershell = 'C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe'
-    command = 'Get-CimInstance -Class Win32_Useraccount -Filter "PasswordRequired=False and LocalAccount=True and Disabled=False" | FT Name, PasswordRequired, LocalAccount'
+    command = 'Get-CimInstance -Class Win32_Useraccount -Filter \'PasswordRequired=False and LocalAccount=True and Disabled=False\' | FT Name, PasswordRequired, LocalAccount'
     Facter::Core::Execution.exec(%(#{powershell} -command "#{command}"))
   end
 end
