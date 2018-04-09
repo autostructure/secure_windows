4ff5ce3d57b33aa3ff4d07c16def01ce9e847962
Jack Coleman <jack@autostructure.io>
2018-04-09 10:36:55 -0400

    test

--- manifests/init.pp
+++ manifests/init.pp
@@ -19,7 +19,7 @@ class secure_windows (
   include ::secure_windows::features
   #include ::secure_windows::registry_editor
   #include ::secure_windows::lgpo
-  include ::secure_windows::gpo
+  #include ::secure_windows::gpo
   include ::secure_windows::servicing_level
   include ::secure_windows::ftp_servers
   include ::secure_windows::ntp
