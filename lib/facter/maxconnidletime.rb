ecc042e718eb913fc7ec2743d70ae3e49d52494a
Jack Coleman <jack@autostructure.io>
2018-04-09 11:15:15 -0400

    update to confine fact to DCs only

--- lib/facter/maxconnidletime.rb
+++ lib/facter/maxconnidletime.rb
@@ -3,7 +3,8 @@ require 'facter'
 Facter.add('maxconnidletime') do
   confine operatingsystem: :windows
   setcode do
-    $time = Facter::Core::Execution.execute("dsquery * \"cn=Default Query Policy,cn=Query-Policies,cn=Directory Service,cn=Windows NT,cn=Services,cn=Configuration,dc=example,dc=com\" -attr LDAPAdminLimits")
-    $time.match(/MaxConnIdleTime=(\d*)/)[1]
+    if Facter.value(:windows_server_type) == 'windowsdc'
+      $time = Facter::Core::Execution.execute("dsquery * \"cn=Default Query Policy,cn=Query-Policies,cn=Directory Service,cn=Windows NT,cn=Services,cn=Configuration,dc=example,dc=com\" -attr LDAPAdminLimits")
+      $time.match(/MaxConnIdleTime=(\d*)/)[1]
   end
 end
