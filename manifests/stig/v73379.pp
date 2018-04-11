2d9e78fd8139e0feb4e448033d0692797109d458
Jack Coleman <jack@autostructure.io>
2018-04-11 10:23:04 -0400

    test

--- manifests/stig/v73379.pp
+++ manifests/stig/v73379.pp
@@ -48,15 +48,15 @@ class secure_windows::stig::v73379 (
 
 
 
-      $dsa_database_partition = regsubst($facts['ntds_parameters']['DSA Database file'], '^(.*:).*$', '\1')
-      $shares = $facts['shares']
-      $relevant_shares = $shares.filter |$items| { $items[0] =~ /!SYSVOL/ }
-      notify { 'testestetughei':
-        message => $shares,
-      }
-      notify { 'tesgeirughse':
-        message => $relevant_shares,
-      }
+      # $dsa_database_partition = regsubst($facts['ntds_parameters']['DSA Database file'], '^(.*:).*$', '\1')
+      # $shares = $facts['shares']
+      # $relevant_shares = $shares.filter |$items| { $items[0] =~ /!SYSVOL/ }
+      # notify { 'testestetughei':
+      #   message => $shares,
+      # }
+      # notify { 'tesgeirughse':
+      #   message => $relevant_shares,
+      # }
     }
 
     # Check the 'shares' fact to verify this STIG
