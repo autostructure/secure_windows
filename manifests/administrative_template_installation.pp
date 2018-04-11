0543f07cffa4f58e5d845a3cd8d830f0bce44170
Jeffrey Frelick <jeff@autostructure.io>
2018-04-11 14:28:14 -0400

    removed group param from file resource.

--- manifests/administrative_template_installation.pp
+++ manifests/administrative_template_installation.pp
@@ -7,25 +7,21 @@ define secure_windows::administrative_template_installation {
 
   file { 'c:\Windows\PolicyDefinitions\MSS-Legacy.admx':
     ensure => 'file'
-    group  => ' BUILTIN\\Administrators',
     source => 'puppet:///modules/secure_windows/STIG/v73495/MSS-Legacy.admx',
   }
 
   file { 'c:\Windows\PolicyDefinitions\en-US\MSS-Legacy.adml':
     ensure => 'file'
-    group  => ' BUILTIN\\Administrators',
     source => 'puppet:///modules/secure_windows/STIG/v73495/MSS-Legacy.adml',
   }
 
   file { 'c:\Windows\PolicyDefinitions\SecGuide.admx':
     ensure => 'file'
-    group  => ' BUILTIN\\Administrators',
     source => 'puppet:///modules/secure_windows/STIG/v73495/SecGuide.admx',
   }
 
   file { 'c:\Windows\PolicyDefinitions\SecGuide.adml':
     ensure => 'file'
-    group  => ' BUILTIN\\Administrators',
     source => 'puppet:///modules/secure_windows/STIG/v73495/SecGuide.adml',
   }
 
