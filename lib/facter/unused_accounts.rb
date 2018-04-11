5dfd125132da8d7dc52695aad8c4f7d00bf88f65
Jack Coleman <jack@autostructure.io>
2018-04-11 18:18:39 -0400

    test indentation?

--- lib/facter/unused_accounts.rb
+++ lib/facter/unused_accounts.rb
@@ -8,18 +8,18 @@ Facter.add('unused_accounts') do
     #command = 'Search-ADAccount -AccountInactive -UsersOnly -TimeSpan 35.00::00::00 | Where-Object { $_.Enabled -eq $true } | FT SamAccountName -HideTableHeaders'
     command = <<-HEREDOC
     ([ADSI]('WinNT://{0}' -f $env:COMPUTERNAME)).Children | Where { $_.SchemaClassName -eq 'user' } | ForEach {
-    $user = ([ADSI]$_.Path)
-    $lastLogin = $user.Properties.LastLogin.Value
-    $enabled = ($user.Properties.UserFlags.Value -band 0x2) -ne 0x2
-    $laterdate = (Get-Date).AddDays(35)
-    if ($lastLogin -eq $null) {
-      $lastLogin = 'Never'
-    }
-    if ($enabled -eq 'True') {
-      if ($lastLogin -gt (Get-Date)) {
-        Write-Host $user.Name
+      $user = ([ADSI]$_.Path)
+      $lastLogin = $user.Properties.LastLogin.Value
+      $enabled = ($user.Properties.UserFlags.Value -band 0x2) -ne 0x2
+      $laterdate = (Get-Date).AddDays(35)
+      if ($lastLogin -eq $null) {
+        $lastLogin = 'Never'
       }
-    }
+      if ($enabled -eq 'True') {
+        if ($lastLogin -gt $laterdate) {
+          Write-Host $user.Name
+        }
+      }
     }
     HEREDOC
     Facter::Core::Execution.exec("#{powershell} -command \"#{command}\"")
