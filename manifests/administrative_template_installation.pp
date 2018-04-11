a6b9b8a916a1a78b72c183e3084602c395e29e01
Jeffrey Frelick <jeff@autostructure.io>
2018-04-11 17:15:48 -0400

    puppet fileserver is case sensitive, filename had lowercase/uppercase mix up.

--- manifests/administrative_template_installation.pp
+++ manifests/administrative_template_installation.pp
@@ -5,39 +5,32 @@
 #
 class secure_windows::administrative_template_installation {
 
-  #file { 'c:\Windows\PolicyDefinitions\MSS-Legacy.admx':
-  #  ensure => 'file',
-  #  mode   => '0660',
-  #  group  => 'BUILTIN\\Administrators',
-  #  source => 'https://192.168.5.62/modules/secure_windows/STIG/v73495/MSS-Legacy.admx',
-  #}
-
   file { 'c:\Windows\PolicyDefinitions\MSS-legacy.admx':
     ensure => 'file',
-    mode   => '0777',
+    mode   => '0660',
+    group  => 'BUILTIN\\Administrators',
     source => 'puppet:///modules/secure_windows/stig/v73495/MSS-legacy.admx',
   }
-  #  source => 'puppet:///modules/secure_windows/MSS-Legacy.admx',
 
-  #file { 'c:\Windows\PolicyDefinitions\en-US\MSS-Legacy.adml':
-  #  ensure => 'file',
-  #  mode   => '0660',
-  #  group  => 'BUILTIN\\Administrators',
-  #  source => 'puppet:///modules/secure_windows/STIG/v73495/MSS-Legacy.adml',
-  #}
+  file { 'c:\Windows\PolicyDefinitions\en-US\MSS-legacy.adml':
+    ensure => 'file',
+    mode   => '0660',
+    group  => 'BUILTIN\\Administrators',
+    source => 'puppet:///modules/secure_windows/STIG/v73495/MSS-legacy.adml',
+  }
 
-  #file { 'c:\Windows\PolicyDefinitions\SecGuide.admx':
-  #  ensure => 'file',
-  #  mode   => '0660',
-  #  group  => 'BUILTIN\\Administrators',
-  #  source => 'puppet:///modules/secure_windows/STIG/v73495/SecGuide.admx',
-  #}
+  file { 'c:\Windows\PolicyDefinitions\SecGuide.admx':
+    ensure => 'file',
+    mode   => '0660',
+    group  => 'BUILTIN\\Administrators',
+    source => 'puppet:///modules/secure_windows/STIG/v73495/SecGuide.admx',
+  }
 
-  #file { 'c:\Windows\PolicyDefinitions\SecGuide.adml':
-  #  ensure => 'file',
-  #  mode   => '0660',
-  #  group  => 'BUILTIN\\Administrators',
-  #  source => 'puppet:///modules/secure_windows/STIG/v73495/SecGuide.adml',
-  #}
+  file { 'c:\Windows\PolicyDefinitions\SecGuide.adml':
+    ensure => 'file',
+    mode   => '0660',
+    group  => 'BUILTIN\\Administrators',
+    source => 'puppet:///modules/secure_windows/STIG/v73495/SecGuide.adml',
+  }
 
 }
