593d7c4f2e8ae398bea06aa9d9eadff463d9b3b3
Jeffrey Frelick <jeff@autostructure.io>
2018-04-11 14:38:45 -0400

    was missing a comma

--- manifests/administrative_template_installation.pp
+++ manifests/administrative_template_installation.pp
@@ -6,28 +6,28 @@
 class secure_windows::administrative_template_installation {
 
   file { 'c:\Windows\PolicyDefinitions\MSS-Legacy.admx':
-    ensure => 'file'
-    owner  => 'Administrator',
-    group  => 'Administrators',
+    ensure => 'file',
+    mode   => '0660',
+    group  => 'BUILTIN\\Administrators',
     source => 'puppet:///modules/secure_windows/STIG/v73495/MSS-Legacy.admx',
   }
 
   #file { 'c:\Windows\PolicyDefinitions\en-US\MSS-Legacy.adml':
-  #  ensure => 'file'
+  #  ensure => 'file',
   #  mode   => '0660',
   #  group  => 'BUILTIN\\Administrators',
   #  source => 'puppet:///modules/secure_windows/STIG/v73495/MSS-Legacy.adml',
   #}
 
   #file { 'c:\Windows\PolicyDefinitions\SecGuide.admx':
-  #  ensure => 'file'
+  #  ensure => 'file',
   #  mode   => '0660',
   #  group  => 'BUILTIN\\Administrators',
   #  source => 'puppet:///modules/secure_windows/STIG/v73495/SecGuide.admx',
   #}
 
   #file { 'c:\Windows\PolicyDefinitions\SecGuide.adml':
-  #  ensure => 'file'
+  #  ensure => 'file',
   #  mode   => '0660',
   #  group  => 'BUILTIN\\Administrators',
   #  source => 'puppet:///modules/secure_windows/STIG/v73495/SecGuide.adml',
