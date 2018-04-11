87aaada8ba478227169745247c706c250db809fa
Jack Coleman <jack@autostructure.io>
2018-04-11 10:42:49 -0400

    V-73379

--- manifests/stig/v73379.pp
+++ manifests/stig/v73379.pp
@@ -10,24 +10,16 @@ class secure_windows::stig::v73379 (
       $hash_of_shares = $facts['shares']
       $hash_of_shares.each |$name,$path| {
         if $name =~ /^SYSVOL/ {
-          notify { 'erguh':
-            message => 'There is a SYSVOL share',
-          }
+
         }
         elsif $name =~ /^NETLOGON/ {
-          notify { 'erguh2':
-            message => 'There is a NETLOGON share',
-          }
+
         }
         elsif $name =~ /^ADMIN\$/ {
-          notify { 'erguh3':
-            message => 'There is a ADMIN$ share',
-          }
+
         }
         elsif $name =~ /^.{1}\$/ {
-          notify { $name:
-            message => 'There is a C$ or D$ share',
-          }
+
         }
         else {
           if $path =~ $dsa_database_partition {
@@ -37,28 +29,6 @@ class secure_windows::stig::v73379 (
           }
         }
       }
-
-
-
-
-
-
-
-
-
-
-
-      # $dsa_database_partition = regsubst($facts['ntds_parameters']['DSA Database file'], '^(.*:).*$', '\1')
-      # $shares = $facts['shares']
-      # $relevant_shares = $shares.filter |$items| { $items[0] =~ /!SYSVOL/ }
-      # notify { 'testestetughei':
-      #   message => $shares,
-      # }
-      # notify { 'tesgeirughse':
-      #   message => $relevant_shares,
-      # }
     }
-
-    # Check the 'shares' fact to verify this STIG
   }
 }
