c209dcb456a31759ef449a187fc97e2ac1ca57ef
Jeffrey Frelick <jeff@autostructure.io>
2018-04-11 17:17:37 -0400

    uppercase directory name in code caused fileserver problem

--- manifests/administrative_template_installation.pp
+++ manifests/administrative_template_installation.pp
@@ -16,21 +16,21 @@ class secure_windows::administrative_template_installation {
     ensure => 'file',
     mode   => '0660',
     group  => 'BUILTIN\\Administrators',
-    source => 'puppet:///modules/secure_windows/STIG/v73495/MSS-legacy.adml',
+    source => 'puppet:///modules/secure_windows/stig/v73495/MSS-legacy.adml',
   }
 
   file { 'c:\Windows\PolicyDefinitions\SecGuide.admx':
     ensure => 'file',
     mode   => '0660',
     group  => 'BUILTIN\\Administrators',
-    source => 'puppet:///modules/secure_windows/STIG/v73495/SecGuide.admx',
+    source => 'puppet:///modules/secure_windows/stig/v73495/SecGuide.admx',
   }
 
   file { 'c:\Windows\PolicyDefinitions\SecGuide.adml':
     ensure => 'file',
     mode   => '0660',
     group  => 'BUILTIN\\Administrators',
-    source => 'puppet:///modules/secure_windows/STIG/v73495/SecGuide.adml',
+    source => 'puppet:///modules/secure_windows/stig/v73495/SecGuide.adml',
   }
 
 }
