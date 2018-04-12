3eb1f1deffa2795d0540f202c9e360bf5be9d373
Jack Coleman <jack@autostructure.io>
2018-04-12 11:31:41 -0400

    test

--- lib/facter/unused_accounts.rb
+++ lib/facter/unused_accounts.rb
@@ -4,34 +4,13 @@
 Facter.add('unused_accounts') do
   confine operatingsystem: :windows
   setcode do
+    command = ''
+    powershell = 'C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe'
     if Facter.value(:windows_server_type) == 'windowsdc'
-      powershell = 'C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe'
       command = 'Search-ADAccount -AccountInactive -UsersOnly -TimeSpan 35.00::00::00 | Where-Object { $_.Enabled -eq $true } | FT SamAccountName -HideTableHeaders'
-      Facter::Core::Execution.exec("#{powershell} -command \"#{command}\"")
     else
-      # require 'win32ole'
-      # users = []
-      # wmi = WIN32OLE.connect("winmgmts:\\\\.\\root\\cimv2")
-      # wmi.ExecQuery("SELECT * FROM Win32_UserAccount").each do |user|
-      #   if user.Disabled == false
-      #     users.push(user.Name)
-      #   end
-      # end
-      # users
-      powershell = 'C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe'
-      #command = 'Search-ADAccount -AccountInactive -UsersOnly -TimeSpan 35.00::00::00 | Where-Object { $_.Enabled -eq $true } | FT SamAccountName -HideTableHeaders'
       command = "([ADSI]('WinNT://{0}' -f $env:COMPUTERNAME)).Children | Where { $_.SchemaClassName -eq 'user' } | ForEach { if (((([ADSI]$_.Path).Properties.UserFlags.Value -band 0x2) -ne 0x2) -eq 'True') { if ((([ADSI]$_.Path).Properties.LastLogin.Value) -lt (Get-Date).AddDays(-1)) {Write-Host ([ADSI]$_.Path).Name} } }"
-      #   $user = ([ADSI]$_.Path)
-      #   $lastLogin = $user.Properties.LastLogin.Value
-      #   $enabled = ($user.Properties.UserFlags.Value -band 0x2) -ne 0x2
-      #   $laterdate = (Get-Date).AddDays(35)
-      #   if ($enabled -eq 'True') {
-      #     if ($lastLogin -gt $laterdate) {
-      #       Write-Host $user.Name
-      #     }
-      #   }
-      # }
-      Facter::Core::Execution.exec("#{powershell} -command \"#{command}\"")
     end
+    Facter::Core::Execution.exec("#{powershell} -command \"#{command}\"")
   end
 end
