62068d90260a1146cd634cf8f2cdef7d1c2b919e
Jack Coleman <jack@autostructure.io>
2018-04-12 12:17:07 -0400

    test

--- manifests/stig/v73647.pp
+++ manifests/stig/v73647.pp
@@ -1,6 +1,7 @@
 # V-73647
 # The required legal notice must be configured to display before console logon.
 class secure_windows::stig::v73647 (
+  String $legalnoticetext,
   Boolean $enforced = false,
 ) {
 
@@ -11,7 +12,7 @@ class secure_windows::stig::v73647 (
     # Deviations are not permitted except as authorized by the
     # Deputy Assistant Secretary of Defense for Information and Identity Assurance.
 
-    $legalnoticetext = 'I\'ve read & consent to terms in IS user agreement.'
+    #$legalnoticetext = 'I\'ve read & consent to terms in IS user agreement.'
 
     registry::value { 'v73647':
       key   => 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System',
