8f822caadefaf0a6c45f6f1d2458f22714a9f636
Jeffrey Frelick <jeff@autostructure.io>
2018-04-12 16:18:54 -0400

    more work on v73513

--- manifests/stig/v73513.pp
+++ manifests/stig/v73513.pp
@@ -10,6 +10,12 @@ class secure_windows::stig::v73513 (
     # Windows Defender Credential Guard only supported on:
     # Windows Server 2016 & Windows 10
     if $facts['operatingsystemmajrelease'] in ['2012 R2','2016','2016 R2'] {
+
+      $test1 = $facts['credential_guard_requiredsecurityproperties'] ? {
+        /2/     => true,
+        default => false,
+      }
+
       notify {"facts['credential_guard_requiredsecurityproperties'] = ${facts['credential_guard_requiredsecurityproperties']}": }
     } else {
       notify {"secure_windows: skipping v-73513, only relevant on Windows Server 2016, operating system (${facts['operatingsystemmajrelease']}) detected.": }
