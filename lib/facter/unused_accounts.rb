16cf829bc07c319973a8c81fcc17085b2666c1ca
Jack Coleman <jack@autostructure.io>
2018-04-12 11:05:58 -0400

    test

--- lib/facter/unused_accounts.rb
+++ lib/facter/unused_accounts.rb
@@ -9,34 +9,29 @@ Facter.add('unused_accounts') do
       command = 'Search-ADAccount -AccountInactive -UsersOnly -TimeSpan 35.00::00::00 | Where-Object { $_.Enabled -eq $true } | FT SamAccountName -HideTableHeaders'
       Facter::Core::Execution.exec("#{powershell} -command \"#{command}\"")
     else
-      require 'win32ole'
-      users = []
-      wmi = WIN32OLE.connect("winmgmts:\\\\.\\root\\cimv2")
-      wmi.ExecQuery("SELECT * FROM Win32_UserAccount").each do |user|
-        if user.Disabled == FALSE
-          users.push(user.Name)
-        end
-      end
-      users
+      # require 'win32ole'
+      # users = []
+      # wmi = WIN32OLE.connect("winmgmts:\\\\.\\root\\cimv2")
+      # wmi.ExecQuery("SELECT * FROM Win32_UserAccount").each do |user|
+      #   if user.Disabled == false
+      #     users.push(user.Name)
+      #   end
+      # end
+      # users
+      powershell = 'C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe'
+      #command = 'Search-ADAccount -AccountInactive -UsersOnly -TimeSpan 35.00::00::00 | Where-Object { $_.Enabled -eq $true } | FT SamAccountName -HideTableHeaders'
+      command = "([ADSI]('WinNT://{0}' -f $env:COMPUTERNAME)).Children | Where { $_.SchemaClassName -eq 'user' } | ForEach { if (($user.Properties.UserFlags.Value -band 0x2) -ne 0x2) -eq 'True') { if (($user.Properties.LastLogin.Value) -gt ((Get-Date).AddDays(35))) {Write-Host $user.Name} } }"
+      #   $user = ([ADSI]$_.Path)
+      #   $lastLogin = $user.Properties.LastLogin.Value
+      #   $enabled = ($user.Properties.UserFlags.Value -band 0x2) -ne 0x2
+      #   $laterdate = (Get-Date).AddDays(35)
+      #   if ($enabled -eq 'True') {
+      #     if ($lastLogin -gt $laterdate) {
+      #       Write-Host $user.Name
+      #     }
+      #   }
+      # }
+      Facter::Core::Execution.exec("#{powershell} -command \"#{command}\"")
     end
-    # powershell = 'C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe'
-    # #command = 'Search-ADAccount -AccountInactive -UsersOnly -TimeSpan 35.00::00::00 | Where-Object { $_.Enabled -eq $true } | FT SamAccountName -HideTableHeaders'
-    # command = <<-HEREDOC
-    # ([ADSI]('WinNT://{0}' -f $env:COMPUTERNAME)).Children | Where { $_.SchemaClassName -eq 'user' } | ForEach {
-    #   $user = ([ADSI]$_.Path)
-    #   $lastLogin = $user.Properties.LastLogin.Value
-    #   $enabled = ($user.Properties.UserFlags.Value -band 0x2) -ne 0x2
-    #   $laterdate = (Get-Date).AddDays(35)
-    #   if ($lastLogin -eq $null) {
-    #     $lastLogin = 'Never'
-    #   }
-    #   if ($enabled -eq 'True') {
-    #     if ($lastLogin -gt $laterdate) {
-    #       Write-Host $user.Name
-    #     }
-    #   }
-    # }
-    # HEREDOC
-    # Facter::Core::Execution.exec("#{powershell} -command \"#{command}\"")
   end
 end
