e1e133259d3ddbb84f0a46ce95b574a50e96b739
Jeffrey Frelick <jeff@autostructure.io>
2018-04-16 10:53:36 -0400

    Changed credential guard fact to report running/not running instead of running/disabled.

--- lib/facter/credential_guard_virtualizationbasedsecuritystatus.rb
+++ lib/facter/credential_guard_virtualizationbasedsecuritystatus.rb
@@ -8,6 +8,6 @@ Facter.add(:credential_guard_virtualizationbasedsecuritystatus) do
     powershell = File.exist?("#{ENV['SYSTEMROOT']}\\system32\\windowspowershell\\v1.0\\powershell.exe") ? "#{ENV['SYSTEMROOT']}\\system32\\windowspowershell\\v1.0\\powershell.exe" : 'powershell.exe'
     command = 'Get-CimInstance -ClassName Win32_DeviceGuard -Namespace root\Microsoft\Windows\DeviceGuard | select VirtualizationBasedSecurityStatus | FT -HideTableHeaders'
     virtualizationbasedsecuritystatus = Facter::Core::Execution.exec(%(#{powershell} -command "#{command}"))
-    (!virtualizationbasedsecuritystatus.empty? && virtualizationbasedsecuritystatus == 2) ? 'running' : 'disabled'
+    (!virtualizationbasedsecuritystatus.empty? && virtualizationbasedsecuritystatus == 2) ? 'running' : 'not running'
   end
 end
