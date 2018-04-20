a5fa46d5e96d659a765ab5ed3485391d7ee7fb72
Jack Coleman <jack@autostructure.io>
2018-04-20 10:18:18 -0400

    test

--- manifests/stig/v73411.pp
+++ manifests/stig/v73411.pp
@@ -5,40 +5,40 @@ class secure_windows::stig::v73411 (
   Boolean $enforced = false,
 ) {
   if $enforced {
-    acl { 'C:\\Windows\\System32\\Eventvwr.exe':
-      inherit_parent_permissions => false,
-      permissions                => [
-        {
-          'identity' => 'NT SERVICE\\TrustedInstaller',
-          'rights'   => ['full'],
-          'affects'  => 'self_only',
-        },
-        {
-          'identity' => 'S-1-5-32-544',
-          'rights'   => ['read', 'execute'],
-          'affects'  => 'self_only',
-        },
-        {
-          'identity' => 'S-1-5-18',
-          'rights'   => ['read', 'execute'],
-          'affects'  => 'self_only',
-        },
-        {
-          'identity' => 'S-1-5-32-545',
-          'rights'   => ['read', 'execute'],
-          'affects'  => 'self_only',
-        },
-        {
-          'identity' => 'S-1-15-2-1',
-          'rights'   => ['read', 'execute'],
-          'affects'  => 'self_only',
-        },
-        {
-          'identity' => 'S-1-15-2-2',
-          'rights'   => ['read', 'execute'],
-          'affects'  => 'self_only',
-        }
-      ],
-    }
+    # acl { 'C:\\Windows\\System32\\Eventvwr.exe':
+    #   inherit_parent_permissions => false,
+    #   permissions                => [
+    #     {
+    #       'identity' => 'NT SERVICE\\TrustedInstaller',
+    #       'rights'   => ['full'],
+    #       'affects'  => 'self_only',
+    #     },
+    #     {
+    #       'identity' => 'S-1-5-32-544',
+    #       'rights'   => ['read', 'execute'],
+    #       'affects'  => 'self_only',
+    #     },
+    #     {
+    #       'identity' => 'S-1-5-18',
+    #       'rights'   => ['read', 'execute'],
+    #       'affects'  => 'self_only',
+    #     },
+    #     {
+    #       'identity' => 'S-1-5-32-545',
+    #       'rights'   => ['read', 'execute'],
+    #       'affects'  => 'self_only',
+    #     },
+    #     {
+    #       'identity' => 'S-1-15-2-1',
+    #       'rights'   => ['read', 'execute'],
+    #       'affects'  => 'self_only',
+    #     },
+    #     {
+    #       'identity' => 'S-1-15-2-2',
+    #       'rights'   => ['read', 'execute'],
+    #       'affects'  => 'self_only',
+    #     }
+    #   ],
+    # }
   }
 }
