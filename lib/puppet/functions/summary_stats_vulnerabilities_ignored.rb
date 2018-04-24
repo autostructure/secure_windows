1d6ef26b1117ee55128a979480742bedca79f802
Jeffrey Frelick <jeff@autostructure.io>
2018-04-24 16:00:53 -0400

    fix custom function

--- lib/puppet/functions/summary_stats_vulnerabilities_ignored.rb
+++ lib/puppet/functions/summary_stats_vulnerabilities_ignored.rb
@@ -1,13 +1,13 @@
 # Puppet server function that reads the appropriate hiera file to
 # count the number of vulnerabilities disabled by the user.
+#
+# dispatch :summary_stats_vulnerabilities_ignored do
+#   return_type 'String'
+# end
 
-Puppet::Functions.create_function(:'secure_windows::vulnerabilities_ignored') do
-  dispatch :vulnerabilities_ignored do
-    return_type 'String'
-  end
-
-  def vulnerabilities_ignored
-    Puppet.notice 'summary_stats_vulnerabilites_ignored.rb ...'
+Puppet::Functions.create_function(:'secure_windows::summary_stats_vulnerabilities_ignored') do
+  def summary_stats_vulnerabilities_ignored
+    Puppet.debug 'summary_stats_vulnerabilites_ignored.rb ...'
     # hiera_lookup_value2 = HieraPuppet.lookup('secure_windows::stig::', false, [], nil, :priority)
     HieraPuppet.lookup('secure_windows::is_dod', false, [], nil, :priority)
   end
