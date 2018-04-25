e319249e45b3974ac872c60d44aedef8a3fefdd6
Jeffrey Frelick <jeff@autostructure.io>
2018-04-25 15:39:12 -0400

    change summary stat to hash

--- lib/facter/summary_stats.rb
+++ lib/facter/summary_stats.rb
@@ -5,13 +5,12 @@ Facter.add(:summary_stats) do
   confine operatingsystem: :windows
 
   setcode do
-    "Module Summary:
-------------------------------------------------------
-252 Vulnerabilities Enforced
- 10 Vulnerabilities Requiring Manual Intevention
-  7 Vulnerabilities Disabled in Configuration File
-  1 Error(s)
-======================================================
-270 Total Vulnerabilities"
+    {
+      vulnerabilities_enforced => 252,
+      vulnerabilities_requiring_manual_intervention => 10,
+      vulnerabilities_disabled_in_config_file => 7,
+      errors => 1,
+      total_vulnerabilities => 270
+    }
   end
 end
