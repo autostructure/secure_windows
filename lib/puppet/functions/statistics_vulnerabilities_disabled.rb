8111d9e8c248794a0183acbb3091041b350078c6
Jeffrey Frelick <jeff@autostructure.io>
2018-04-26 10:16:54 -0400

    added exception handling to function

--- lib/puppet/functions/statistics_vulnerabilities_disabled.rb
+++ lib/puppet/functions/statistics_vulnerabilities_disabled.rb
@@ -15,18 +15,26 @@ Puppet::Functions.create_function(:statistics_vulnerabilities_disabled) do
   end
 
   def no_scope
-    scope = closure_scope
-    global_scope = scope.find_global_scope
-    HieraPuppet.lookup('secure_windows::log::enabled', nil, global_scope, nil, :priority)
-    # scope['is_dod']
-    # class_scope = scope.class_scope('secure_windows')
-    # fqdn = scope['facts']['networking']['fqdn']
-    # HieraPuppet.lookup('secure_windows::is_dod', nil, [], nil, :priority)
+    begin
+      scope = closure_scope
+      global_scope = scope.find_global_scope
+      HieraPuppet.lookup('secure_windows::log::enabled', nil, global_scope, nil, :priority)
+      # scope['is_dod']
+      # class_scope = scope.class_scope('secure_windows')
+      # fqdn = scope['facts']['networking']['fqdn']
+      # HieraPuppet.lookup('secure_windows::is_dod', nil, [], nil, :priority)
+    rescue
+      'Hiera lookup failed.'
+    end
   end
 
   def key_value(key)
-    scope = closure_scope
-    HieraPuppet.lookup(key, nil, scope, nil, :priority)
+    begin
+      scope = closure_scope
+      HieraPuppet.lookup(key, nil, scope, nil, :priority)
+    rescue
+      'Hiera lookup failed.'
+    end
   end
 
   # dispatch :supplied_scope do
