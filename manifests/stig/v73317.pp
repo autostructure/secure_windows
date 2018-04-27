db9ba2b8ff1aca72f744789861c01a541c9eb4dd
Jeffrey Frelick <jeff@autostructure.io>
2018-04-27 10:04:20 -0400

    test

--- manifests/stig/v73317.pp
+++ manifests/stig/v73317.pp
@@ -17,8 +17,6 @@ class secure_windows::stig::v73317 (
     #$secure_windows::statistics_vulnerabilities_enforced = $secure_windows::statistics_vulnerabilities_enforced + 1
 
   } else {
-    # also covers v73223
     $secure_windows::statistics_vulnerabilities_disabled = 1
-    #$secure_windows::statistics_vulnerabilities_disabled = $secure_windows::statistics_vulnerabilities_disabled + 1
   }
 }
