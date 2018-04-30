5b26c7fa6ed102afe5c607e1c8aa0c45887fbf4e
Jack Coleman <jack@autostructure.io>
2018-04-30 13:28:33 -0400

    Test warning for non compliance with v73611

--- lib/facter/pki_certs.rb
+++ lib/facter/pki_certs.rb
@@ -1,11 +1,17 @@
 #
-# This fact returns the PKI Personal Certs on the machine
+# This fact returns thumbprints of PKI Personal Certs on the machine
 #
 Facter.add('pki_certs') do
   confine operatingsystem: :windows
   setcode do
     powershell = 'C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe'
-    command = 'Import-Module PKI; set-location cert:\localmachine\My; get-childitem'
-    Facter::Core::Execution.exec(%(#{powershell} -command "#{command}"))
+    command = 'Import-Module PKI; set-location cert:\localmachine\My; get-childitem | FT Thumbprint -HideTableHeaders'
+    result = Facter::Core::Execution.exec(%(#{powershell} -command "#{command}"))
+    none = 'no certs'
+    if result != ''
+      result
+    else
+      none
+    end
   end
 end
---
+++ manifests/stig/v73611.pp
@@ -0,0 +1,14 @@
+# This class managed V-73611
+# Domain controllers must have a PKI server certificate.
+class secure_windows::stig::v73611 (
+  Boolean $enforced = false,
+) {
+  if $enforced {
+    if ($facts['pki_certs'] == 'no certs') {
+      notify { 'v73611':
+        message  => 'Not in compliance with DoD STIG V-73611. The Domain Controller does not have a PKI server certificate.'
+        loglevel => warning,
+      }
+    }
+  }
+}
