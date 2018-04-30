97f3ec0f3344e0c74d5e3eff0d9123700d3e6c92
Jack Coleman <jack@autostructure.io>
2018-04-30 15:23:28 -0400

    test

--- lib/facter/tpm_version.rb
+++ lib/facter/tpm_version.rb
@@ -10,6 +10,11 @@ Facter.add(:tpm_version) do
   setcode do
     powershell = 'C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe'
     command = 'Get-tpm'
-    Facter::Core::Execution.exec(%(#{powershell} -command "#{command}"))
+    result = Facter::Core::Execution.exec(%(#{powershell} -command "#{command}"))
+    if result != ''
+      result
+    else
+      'TPM not enabled'
+    end
   end
 end
