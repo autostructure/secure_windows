3516ccb95d0affae46cca30b7f1620cfdc099cea
Jack Coleman <jack@autostructure.io>
2018-04-30 15:06:57 -0400

    test

--- lib/facter/tpm_version.rb
+++ lib/facter/tpm_version.rb
@@ -8,10 +8,9 @@ require 'facter'
 Facter.add(:tpm_version) do
   confine operatingsystem: :windows
   setcode do
-
     require 'win32ole'
     wmi = WIN32OLE.connect('winmgmts:\\\\.\\root\\cimv2')
     version = wmi.ExecQuery('select SpecVersion from Win32_Tpm')
-    version
+    version.SpecVersion
   end
 end
