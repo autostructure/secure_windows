a9db490fe761bf511e681604f1149defc67608ee
Jeffrey Frelick <jeff@autostructure.io>
2018-05-01 13:51:08 -0400

    j

--- lib/puppet/functions/statistics.rb
+++ lib/puppet/functions/statistics.rb
@@ -17,14 +17,13 @@ Puppet::Functions.create_function(:statistics) do
   def no_args
     begin
       curl_cmd = "curl -G 'http://localhost:8080/pdb/query/v4/event-counts' --data-urlencode 'query=[\"~\", \"containing_class\", \"Secure_windows::Stig::[vV]\\d{5}\"]' --data-urlencode 'summarize_by=containing_class' --data-urlencode 'count_by=certname'"
-      class_summary = Facter::Core::Execution.exec(curl_cmd)
+      Facter::Core::Execution.execute(curl_cmd,{})
       # class_array = Facter::Core::Execution.exec(curl_cmd).split("\n")
       # class_array.each do |classname|
         # classname['success'] =
         # classname['failed'] =
         # classname['skipped'] =
       # end
-      class_summary
     rescue Exception => e
       "PuppetDB statistics query failed, error message:\n#{e.message}\n#{e.backtrace.inspect}"
     end
