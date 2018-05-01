c87bae84a7cd04b90717de0cf0bc9ca682adcaa0
Jeffrey Frelick <jeff@autostructure.io>
2018-05-01 14:23:20 -0400

    TEST

--- lib/puppet/functions/statistics.rb
+++ lib/puppet/functions/statistics.rb
@@ -22,8 +22,18 @@ Puppet::Functions.create_function(:statistics) do
       #cmd_args << "--data-urlencode 'summarize_by=containing_class'"
       #cmd_args << "--data-urlencode 'count_by=certname'"
       #curl_cmd = 'curl'
-      curl_cmd = "curl -G 'http://localhost:8080/pdb/query/v4/event-counts' --data-urlencode 'query=[\"~\", \"containing_class\", \"Secure_windows::Stig::[vV]\\d{5}\"]' --data-urlencode 'summarize_by=containing_class' --data-urlencode 'count_by=certname'"
-      Puppet::Util::Execution.execute(curl_cmd, {})
+
+      cmd_args = {
+        data-urlencode => "\'query=[\"~\", \"containing_class\", \"Secure_windows::Stig::[vV]\\d{5}\"]\'",
+      }
+      curl_cmd = "curl -G 'http://localhost:8080/pdb/query/v4/event-counts' --data-urlencode 'summarize_by=containing_class' --data-urlencode 'count_by=certname'"
+      Puppet::Util::Execution.execute(curl_cmd, cmd_args)
+
+
+      #curl_cmd = "curl -G 'http://localhost:8080/pdb/query/v4/event-counts' --data-urlencode 'query=[\"~\", \"containing_class\", \"Secure_windows::Stig::[vV]\\d{5}\"]' --data-urlencode 'summarize_by=containing_class' --data-urlencode 'count_by=certname'"
+      #Puppet::Util::Execution.execute(curl_cmd, {})
+
+
       # class_array = Facter::Core::Execution.exec(curl_cmd).split("\n")
       # class_array.each do |classname|
         # classname['success'] =
