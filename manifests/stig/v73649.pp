3f0144add56e4810eb9330d1831bef996832e83a
Jack Coleman <jackclmn@umich.edu>
2018-05-07 12:59:58 -0400

    Add parameter for legalnoticetitle in v73649 (#8)

--- manifests/stig/v73649.pp
+++ manifests/stig/v73649.pp
@@ -2,14 +2,13 @@
 # The Windows dialog box title for the legal banner must be configured with the appropriate text.
 class secure_windows::stig::v73649 (
   Boolean $enforced = false,
+  String $legalnoticetitle = 'US Department of Defense Warning Statement'
 ) {
 
   if $enforced {
 
     # Alternatively, the title below is also valid...
     # $legalnoticetitle = 'DoD Notice and Consent Banner'
-
-    $legalnoticetitle = 'US Department of Defense Warning Statement'
 
     registry::value { 'v73649':
       key   => 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System',
