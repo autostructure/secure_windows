859bf911e88eefe9c6140c47cedc9c97167481ee
Jack Coleman <jack@autostructure.io>
2018-05-10 11:42:29 -0400

    test

--- manifests/stig/v43241.pp
+++ manifests/stig/v43241.pp
@@ -7,11 +7,19 @@ class secure_windows::stig::v43241 (
   #   if $facts['operatingsystemmajrelease'] == '2012 R2' {
   #     registry::value { 'v43241':
   #       key   => 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System',
-  #       value => 'test',
+  #       value => 'MSAOptional',
   #       type  => 'dword',
   #       data  => '0x00000001',
   #     }
   #   }
   # }
   # Known bug that has been submitted to Puppet with no fix
+  registry_key { 'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System':
+    ensure => present,
+  }
+  registry_value { 'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System\MSAOptional':
+    ensure => present,
+    type   => dword,
+    data   => '0x00000001',
+  }
 }
