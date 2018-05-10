58ee7d04c08eb0341f2c798a70dbada9f18b0535
Jack Coleman <jack@autostructure.io>
2018-05-10 11:11:38 -0400

    test

--- manifests/init.pp
+++ manifests/init.pp
@@ -54,7 +54,7 @@ class secure_windows (
       class { '::secure_windows::stig::v42420': }
       class { '::secure_windows::stig::v43238': }
       class { '::secure_windows::stig::v43239': }
-      # class { '::secure_windows::stig::v43240': }
+      class { '::secure_windows::stig::v43240': }
       # class { '::secure_windows::stig::v43241': }
       # class { '::secure_windows::stig::v43245': }
       # class { '::secure_windows::stig::v57633': }
