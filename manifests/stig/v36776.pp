448ca40b9e2862d3b2588b9a44b2ce8c1ce8d43a
Jack Coleman <jack@autostructure.io>
2018-05-10 10:19:32 -0400

    hkcu not supported. removing for now

--- manifests/stig/v36776.pp
+++ manifests/stig/v36776.pp
@@ -3,12 +3,13 @@
 class secure_windows::stig::v36776 (
   Boolean $enforced = true,
 ) {
-  if $enforced {
-    registry::value { 'v36776':
-      key   => 'HKEY_CURRENT_USER\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\PushNotifications',
-      value => 'NoCloudApplicationNotification',
-      type  => 'dword',
-      data  => '0x00000001',
-    }
-  }
+  # if $enforced {
+  #   registry::value { 'v36776':
+  #     key   => 'HKEY_CURRENT_USER\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\PushNotifications',
+  #     value => 'NoCloudApplicationNotification',
+  #     type  => 'dword',
+  #     data  => '0x00000001',
+  #   }
+  # }
+  #hkcu not supported :(
 }
--- manifests/stig/v36777.pp
+++ manifests/stig/v36777.pp
@@ -3,12 +3,13 @@
 class secure_windows::stig::v36777 (
   Boolean $enforced = true,
 ) {
-  if $enforced {
-    registry::value { 'v36777':
-      key   => 'HKEY_CURRENT_USER\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\PushNotifications',
-      value => 'NoToastApplicationNotificationOnLockScreen',
-      type  => 'dword',
-      data  => '0x00000001',
-    }
-  }
+  # if $enforced {
+  #   registry::value { 'v36777':
+  #     key   => 'HKEY_CURRENT_USER\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\PushNotifications',
+  #     value => 'NoToastApplicationNotificationOnLockScreen',
+  #     type  => 'dword',
+  #     data  => '0x00000001',
+  #   }
+  # }
+  # hkcu not supported :(
 }
