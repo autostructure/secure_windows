62103dd98cbca658c28c1f337bce30f6da288be0
Jack Coleman <jack@autostructure.io>
2018-05-11 15:26:59 -0400

    k

--- manifests/stig/v4438.pp
+++ manifests/stig/v4438.pp
@@ -4,11 +4,16 @@ class secure_windows::stig::v4438 (
   Boolean $enforced = true,
 ) {
   if $enforced {
-    registry::value { 'v4438':
-      key   => 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters',
-      value => 'TcpMaxDataRetransmissions',
-      type  => 'dword',
-      data  => '0x00000003',
+    # registry::value { 'v4438':
+    #   key   => 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters',
+    #   value => 'TcpMaxDataRetransmissions',
+    #   type  => 'dword',
+    #   data  => '0x00000003',
+    # }
+    registry_value { 'HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\TcpMaxDataRetransmissions':
+      ensure => present,
+      type   => dword,
+      data   => '0x00000003',
     }
   }
 }
