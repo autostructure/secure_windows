e88aaea399ad12c272007cef9d3af5ae82a01338
Jack Coleman <jack@autostructure.io>
2018-05-11 17:28:12 -0400

    v

--- manifests/stig/v1136.pp
+++ manifests/stig/v1136.pp
@@ -3,14 +3,14 @@
 class secure_windows::stig::v1136 (
   Boolean $enforced = true,
 ) {
-  if $enforced {
-    local_security_policy { 'Network security: Force logoff when logon hours expire':
-      ensure         => 'present',
-      policy_setting => 'ForceLogoffWhenHourExpire',
-      policy_type    => 'System Access',
-      policy_value   => '1',
-    }
-
-  }
+  # if $enforced {
+  #   # local_security_policy { 'Network security: Force logoff when logon hours expire':
+  #   #   ensure         => 'present',
+  #   #   policy_setting => 'ForceLogoffWhenHourExpire',
+  #   #   policy_type    => 'System Access',
+  #   #   policy_value   => '1',
+  #   # }
+  #
+  # }
 
 }
