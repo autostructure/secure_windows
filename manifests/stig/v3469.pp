62acb450afb50b09d524a419bea0d8d5046ae450
Jack Coleman <jack@autostructure.io>
2018-05-11 16:44:27 -0400

    r

--- manifests/stig/v3469.pp
+++ manifests/stig/v3469.pp
@@ -4,11 +4,16 @@ class secure_windows::stig::v3469 (
   Boolean $enforced = true,
 ) {
   if $enforced {
-    registry::value { 'v3469':
-      key   => 'HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\system',
-      value => 'DisableBkGndGroupPolicy',
-      type  => 'dword',
-      data  => '0',
+    # registry::value { 'v3469':
+    #   key   => 'HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\system',
+    #   value => 'DisableBkGndGroupPolicy',
+    #   type  => 'dword',
+    #   data  => '0',
+    # }
+    registry_value { 'HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\system\DisableBkGndGroupPolicy':
+      ensure => present,
+      type   => dword,
+      data   => '0x00000000',
     }
   }
 }
