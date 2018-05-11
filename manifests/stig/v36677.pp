05861e935426a2cc23ccbd9da647656b6f39f94a
Jack Coleman <jack@autostructure.io>
2018-05-11 11:35:46 -0400

    fix duplicate resource declaration error

--- manifests/stig/v36677.pp
+++ manifests/stig/v36677.pp
@@ -4,7 +4,7 @@ class secure_windows::stig::v36677 (
   Boolean $enforced = true,
 ) {
   if $enforced {
-    registry::value { 'v36678':
+    registry::value { 'v36677':
       key   => 'HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\Servicing',
       value => 'UseWindowsUpdate',
       type  => 'dword',
