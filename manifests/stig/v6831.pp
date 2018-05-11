d8ca4188e4ff5e3d73ad37ff4d7bafdcd6d5141c
Jack Coleman <jack@autostructure.io>
2018-05-11 11:59:24 -0400

    fix

--- manifests/stig/v6831.pp
+++ manifests/stig/v6831.pp
@@ -4,11 +4,16 @@ class secure_windows::stig::v6831 (
   Boolean $enforced = true,
 ) {
   if $enforced {
-    registry::value { 'v6831':
-      key   => 'HKEY_LOCAL_MACHINE\System\CurrentControlSet\Services\Netlogon\Parameters',
-      value => 'RequireSignOrSeal',
-      type  => 'dword',
-      data  => '0x00000001',
+    # registry::value { 'v6831':
+    #   key   => 'HKEY_LOCAL_MACHINE\System\CurrentControlSet\Services\Netlogon\Parameters',
+    #   value => 'RequireSignOrSeal',
+    #   type  => 'dword',
+    #   data  => '0x00000001',
+    # }
+    registry_value { 'HKLM\System\CurrentControlSet\Services\Netlogon\Parameters\RequireSignOrSeal':
+      ensure => present,
+      type   => dword,
+      data   => '0x00000001',
     }
   }
 }
