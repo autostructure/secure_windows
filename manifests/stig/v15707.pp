078b968ef2d4a8e793b71dcc566fa48dd388a011
Luther <luther@autostructure.io>
2018-05-14 10:31:55 -0400

    Added colon

--- manifests/stig/v15707.pp
+++ manifests/stig/v15707.pp
@@ -5,7 +5,7 @@ class secure_windows::stig::v15707 (
 ) {
   if $enforced {
 
-    registry_value { 'HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows NT\Terminal Services\LoggingEnabled',
+    registry_value { 'HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows NT\Terminal Services\LoggingEnabled':
       ensure => present,
       type  => 'dword',
       data  => '0x00000001',
