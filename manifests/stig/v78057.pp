39f8518ed02349756680d24979a65e42e87f247c
Luther <luther@autostructure.io>
2018-06-05 16:20:23 -0400

    fixing stupid

--- manifests/stig/v78057.pp
+++ manifests/stig/v78057.pp
@@ -3,7 +3,7 @@
 # This class manages V-78059
 # Windows Server 2012/2012 R2 must be configured to audit Logon/Logoff - Account Lockout failures.
 class secure_windows::stig::v78057 (
-  Boolean $enforced = true,
+  Boolean $enforced = false,
 ) {
   if $enforced {
     auditpol { 'Logon':
