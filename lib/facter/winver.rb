701b1dbb9fefd892ebcb6b0b7d386d20a42bf42f
Luther <luther@autostructure.io>
2018-06-07 16:13:58 -0400

    jeff 2.0.1

--- lib/facter/winver.rb
+++ lib/facter/winver.rb
@@ -7,7 +7,7 @@ Facter.add('winver') do
     powershell = 'C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe'
     command = "[environment]::OSVersion.Version | select Major, Minor, Build | FT -HideTableHeaders -AutoSize"
     out = Facter::Core::Execution.execute("#{powershell} -command \"#{command}\"")
-    out.split("\s")
+    out = out.split("\s")
     metric = out[1] + '.' + out[2]
   end
 end
