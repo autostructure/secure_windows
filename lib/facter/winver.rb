f5f29bbc5f5fd92ea549ee749488cab4f930aa4b
Luther <luther@autostructure.io>
2018-06-07 15:00:48 -0400

    blah

--- lib/facter/winver.rb
+++ lib/facter/winver.rb
@@ -4,7 +4,7 @@ Facter.add('winver') do
   confine operatingsystem: :windows
   setcode do
     powershell = 'C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe'
-    command = "winver.exe"
+    command = "[environment]::OSVersion.Version"
     Facter::Core::Execution.execute("#{powershell} -command \"#{command}\"")
   end
 end
