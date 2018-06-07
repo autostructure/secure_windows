b6978ba19a2148cb93f7078f37a4966195bd270c
Luther <luther@autostructure.io>
2018-06-07 14:42:17 -0400

    add "winver" facter

---
+++ lib/facter/winver.rb
@@ -0,0 +1,10 @@
+# This fact returns the ftp root of a FTP Server with a site equal to fqdn
+require 'facter'
+Facter.add('winver') do
+  confine operatingsystem: :windows
+  setcode do
+    powershell = 'C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe'
+    command = "winver.exe"
+    Facter::Core::Execution.execute("#{powershell} -command \"#{command}\"")
+  end
+end
