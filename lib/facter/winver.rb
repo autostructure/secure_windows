0994829f928d01f4c5495f1dd251dd0634048d88
Luther <luther@autostructure.io>
2018-06-07 15:29:48 -0400

    working on hash output from facter measure

--- lib/facter/winver.rb
+++ lib/facter/winver.rb
@@ -2,10 +2,10 @@
 require 'facter'
 Facter.add('winver') do
   confine operatingsystem: :windows
-  out = new Hash
   setcode do
     powershell = 'C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe'
     command = "[environment]::OSVersion.Version | select Major, Minor, Build | FT -HideTableHeaders -AutoSize"
     out = Facter::Core::Execution.execute("#{powershell} -command \"#{command}\"")
+    out.split("\w")
   end
 end
