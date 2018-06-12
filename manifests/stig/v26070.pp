95e30ce598707e23b95641c9714de197169690ff
Luther <luther@autostructure.io>
2018-06-12 13:41:29 -0400

    dfasgdr

--- manifests/stig/v26070.pp
+++ manifests/stig/v26070.pp
@@ -17,41 +17,10 @@ class secure_windows::stig::v26070 (
         'AccessControlType' => 'Allow',
         'IdentityReference' => 'S-1-5-32-545',
         'IsInherited'       => false,
-        'InheritanceFlags'  => 'None',
-        'PropagationFlags'  => 'None'
-      },
-      {
-        'RegistryRights'    => 'FullControl',
-        'AccessControlType' => 'Allow',
-        'IdentityReference' => 'S-1-5-32-544',
-        'IsInherited'       => false,
-        'InheritanceFlags'  => 'None',
-        'PropagationFlags'  => 'None'
-      },
-      {
-        'RegistryRights'    => 'FullControl',
-        'AccessControlType' => 'Allow',
-        'IdentityReference' => 'S-1-5-18',
-        'IsInherited'       => false,
-        'InheritanceFlags'  => 'None',
-        'PropagationFlags'  => 'None'
-      },
-      {
-        'RegistryRights'    => 'FullControl',
-        'AccessControlType' => 'Allow',
-        'IdentityReference' => 'S-1-3-0',
-        'IsInherited'       => false,
         'InheritanceFlags'  => 'ContainerInherit',
-        'PropagationFlags'  => 'InheritOnly'
-      },
-      {
-        'RegistryRights'    => 'ReadKey',
-        'AccessControlType' => 'Allow',
-        'IdentityReference' => 'ALL APPLICATION PACKAGES',
-        'IsInherited'       => false,
-        'InheritanceFlags'  => 'None',
-        'PropagationFlags'  => 'None'
-      }],
+        'PropagationFlags'  => 'InheritOnly',
+      }
+      ],
     }
   }
 }
