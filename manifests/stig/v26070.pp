d6bf9be0e81ed0027aa5fbe937ae9860a8268b33
Luther <luther@autostructure.io>
2018-06-12 14:24:46 -0400

    syntax

--- manifests/stig/v26070.pp
+++ manifests/stig/v26070.pp
@@ -7,7 +7,7 @@ class secure_windows::stig::v26070 (
   if $enforced {
 
     reg_acl { 'vstriker':
-      target              => 'hklm:software\microsoft\windows NT\currentversion\winlogon'
+      target              => 'hklm:software\microsoft\windows NT\currentversion\winlogon',
       inherit_from_parent => true,
       owner               => 'S-1-5-18',
       permissions         => [
