1a01efa69702d994aa3ebb97eb7bb982cc69e6f4
Luther <luther@autostructure.io>
2018-06-15 11:22:49 -0400

    fsaDGzfhgtfygu

--- manifests/stig/v73523.pp
+++ manifests/stig/v73523.pp
@@ -5,13 +5,13 @@ class secure_windows::stig::v73523 (
   Boolean $enforced = true,
 ) {
   if $enforced {
-    registry::value { 'v73523':
+    registry::value { 'v73523_start':
       key   => 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\mrxsmb10',
       value => 'Start',
       type  => 'dword',
       data  => '4',
     }
-    registry::value { 'v73523':
+    registry::value { 'v73523_dependonservice':
       key   => 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\LanmanWorkstation',
       value => 'DependOnService',
       type  => 'array',
