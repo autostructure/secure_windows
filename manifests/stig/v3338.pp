c7e25bd8b1d8e3664d36d857da56758ef9e27ca3
Luther <luther@autostructure.io>
2018-06-18 12:27:07 -0400

    indentation

--- manifests/stig/v3338.pp
+++ manifests/stig/v3338.pp
@@ -3,25 +3,24 @@
 
 class secure_windows::stig::v3338 (
   Boolean $enforced = true,
-) {
+)
+{
   if $enforced {
     if($facts['windows_server_type'] == 'windowsdc') {
-        registry::value { 'v3338':
-          key   => 'HKEY_LOCAL_MACHINE\System\CurrentControlSet\Services\LanManServer\Parameters',
-          value => 'NullSessionPipes',
-          type  => 'string',
-          data  => 'netlogon, samr, lsarpc',
-        }
+      registry::value { 'v3338':
+        key   => 'HKEY_LOCAL_MACHINE\System\CurrentControlSet\Services\LanManServer\Parameters',
+        value => 'NullSessionPipes',
+        type  => 'string',
+        data  => 'netlogon, samr, lsarpc',
       }
-        else {
-          if !($facts['windows_server_type'] == 'windowsdc') {
-            registry::value { 'v3338':
-            key   => 'HKEY_LOCAL_MACHINE\System\CurrentControlSet\Services\LanManServer\Parameters',
-            value => 'NullSessionPipes',
-            type  => 'string',
-            data  => '',
+    }
+    else {
+      registry::value { 'v3338':
+        key   => 'HKEY_LOCAL_MACHINE\System\CurrentControlSet\Services\LanManServer\Parameters',
+        value => 'NullSessionPipes',
+        type  => 'string',
+        data  => '',
       }
     }
   }
-}
 }
