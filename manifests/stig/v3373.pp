8676088df348104d9a17ab8b109893e60d20b10e
Luther <luther@autostructure.io>
2018-06-18 11:10:15 -0400

    reformatted

--- manifests/stig/v3373.pp
+++ manifests/stig/v3373.pp
@@ -5,11 +5,10 @@ class secure_windows::stig::v3373 (
   Boolean $enforced = true,
 ) {
   if $enforced {
-    registry::value { 'v3373':
-      key   => 'HKEY_LOCAL_MACHINE\System\CurrentControlSet\Services\Netlogon\Parameters',
-      value => 'MaximumPasswordAge',
-      type  => 'dword',
-      data  => '30',
+    registry_value { 'HKLM\System\CurrentControlSet\Services\Netlogon\Parameters':
+      ensure => present,
+      type   => 'dword',
+      data   => '30',
     }
 
   }
