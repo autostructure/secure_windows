3df4ad12c849f03bba01dacb8bbe4c46018c29a8
Luther <luther@autostructure.io>
2018-06-19 16:42:03 -0400

    more work

--- lib/facter/password_lastset.rb
+++ lib/facter/password_lastset.rb
@@ -7,7 +7,12 @@ Facter.add('password_lastset') do
   setcode do
     powershell = 'C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe'
     command = 'Net User adminaccount | Find /i "Password Last Set'
-    pwdlastset = Facter::Core::Execution.execute("#{powershell} -command \"#{command}\"")
+    puts "#{command}"
+    pwdlastset = Facter::Core::Execution.execute("#{command}")
 #    pwdlastset.match(/\S+\s\S+\s\S+$/)
   end
 end
+
+
+
+#    pwdlastset = Facter::Core::Execution.execute("#{powershell} -command \"#{command}\"")
