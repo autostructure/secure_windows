b1ef0cab2c6c2227e019bcf58f642749a23725e6
Luther <luther@autostructure.io>
2018-06-19 12:10:08 -0400

    Testing v14268 using HKEY_USERS

--- manifests/stig/v14268.pp
+++ manifests/stig/v14268.pp
@@ -7,7 +7,7 @@ class secure_windows::stig::v14268 (
   if $enforced {
 
     registry::value { 'v14268':
-      key   => 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Attachments',
+      key   => 'HKEY_USERS\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Attachments',
       value => 'SaveZoneInformation',
       type  => 'dword',
       data  => '0x00000002',
