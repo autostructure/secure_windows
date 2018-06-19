b4e333dfa070ce60235c35e9d31731af3c26d7ae
Luther <luther@autostructure.io>
2018-06-19 16:22:07 -0400

    created password_lastset

---
+++ lib/facter/password_lastset.rb
@@ -0,0 +1,13 @@
+#
+# This fact returns the last date the password was changed
+#
+
+Facter.add('password_lastset') do
+  confine operatingsystem: :windows
+  setcode do
+    powershell = 'C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe'
+    command = 'Net User adminaccount | Find /i "Password Last Set'
+    pwdlastset = Facter::Core::Execution.exec(%(#{powershell} -command "#{command}"))
+    pwdlastset.match(/\S+\s\S+\s\S+$/)
+  end
+end
