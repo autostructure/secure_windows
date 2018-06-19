48c712b328070fcbe23243df773b7bec6050fb19
Luther <luther@autostructure.io>
2018-06-19 09:51:05 -0400

    v3339

---
+++ manifests/stig/v3339.pp
@@ -0,0 +1,19 @@
+# This class manages V-3339
+# Unauthorized remotely accessible registry paths must not be configured.
+
+
+class secure_windows::stig::v3339 (
+  Boolean $enforced = true,
+)
+{
+  if $enforced {
+      registry::value { 'v3339':
+        key   => 'HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\SecurePipeServers\Winreg\AllowedExactPaths',
+        value => 'Machine',
+        type  => 'array',
+        data  => ['System\CurrentControlSet\Control\ProductOptions','System\CurrentControlSet\Control\Server Applications',
+        'Software\Microsoft\Windows NT\CurrentVersion'],
+      }
+    }
+  }
+}
