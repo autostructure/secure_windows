ba8a968c7813a25fa483c9c95c55da8f5affc279
Luther <luther@autostructure.io>
2018-06-20 13:11:36 -0400

    compare 2 dates

--- lib/facter/password_lastset.rb
+++ lib/facter/password_lastset.rb
@@ -11,21 +11,22 @@ Facter.add('password_lastset') do
     getdate = Facter::Core::Execution.exec(%(#{powershell} -command "#{command}"))
     currdate = getdate.match(/\d+\/\d+\/\d+/).to_s
     puts "currdate string=#{currdate}"
-    date1 = currdate.to_d
+    date1 = Date.parse('2018-06-20')
     puts "currdate date=#{date1}"
 
     command = "Net User adminaccount | Find /i \"Password Last Set\""
     getdate = Facter::Core::Execution.execute("#{command}")
     pwdlastset = getdate.match(/\d+\/\d+\/\d+/).to_s
     puts "pwdlastesetdate match=#{pwdlastset}"
-    date2 = pwdlastset.to_d
+    date2 = Date.parse('2018-05-30')
     puts "pwdlastesetdate date=#{date2}"
     #pwdlastset = Date.parse(getdate.to_s)
 
-    puts date2 - date1
-    puts Date.today.between?(date1, date2)
+    days = date2 - date1
+    puts "days=#{days}"
+    #puts Date.today.between?(date1, date2)
 
-    5
+    days
 
   end
 end
