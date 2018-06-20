46a12ae7834f0c18242f7eddf855e04f02d9d605
Luther <luther@autostructure.io>
2018-06-20 12:32:58 -0400

    fixing

--- lib/facter/password_lastset.rb
+++ lib/facter/password_lastset.rb
@@ -1,19 +1,22 @@
 #
 # This fact returns the last date the password was changed
 #
-
+require 'date'
 Facter.add('password_lastset') do
   confine operatingsystem: :windows
   setcode do
     powershell = 'C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe'
 
     command = 'Get-Date -Format g'
-    currdate = Facter::Core::Execution.exec(%(#{powershell} -command "#{command}"))
-    currdate = currdate.match(/\d+\/\d+\/\d+/)
+    getdate = Facter::Core::Execution.exec(%(#{powershell} -command "#{command}")).match(/\d+\/\d+\/\d+/)
+    currdate = Date.parse(getdate)
 
     command = "Net User adminaccount | Find /i \"Password Last Set\""
-    pwdlastset = Facter::Core::Execution.execute("#{command}")
-    pwdlastset = pwdlastset.match(/\d+\/\d+\/\d+/)
+    getdate = Facter::Core::Execution.execute("#{command}").match(/\d+\/\d+\/\d+/)
+    pwdlastset = Date.parse(getdate)
+
+    puts pwdlastset - currdate
+    puts Date.today.between?(currdate, pwdlastset)
 
     pwdlastset - currdate
 
