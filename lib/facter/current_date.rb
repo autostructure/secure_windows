ca2879a6feb54451f7fb94533d069584e799de73
Luther <luther@autostructure.io>
2018-06-20 10:20:09 -0400

    created current_date.rb

---
+++ lib/facter/current_date.rb
@@ -0,0 +1,10 @@
+# Echos the current date. Super complex.
+
+Facter.add('current_date') do
+  confine operatingsystem: :windows
+  setcode do
+    powershell = 'C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe'
+    command = 'get-date -format g'
+    Facter::Core::Execution.exec(%(#{powershell} -command "#{command}"))
+  end
+end
