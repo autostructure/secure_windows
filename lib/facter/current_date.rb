4b0afd9cea71d029cfcc44000e0f7e6c1a327d4e
Luther <luther@autostructure.io>
2018-06-20 11:03:39 -0400

    edfg

--- lib/facter/current_date.rb
+++ lib/facter/current_date.rb
@@ -5,7 +5,7 @@ Facter.add('current_date') do
   setcode do
     powershell = 'C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe'
     command = 'Get-Date -Format g'
-    Facter::Core::Execution.exec(%(#{powershell} -command "#{command}"))
+    currdate = Facter::Core::Execution.exec(%(#{powershell} -command "#{command}"))
     currdate.match(/........./)
   end
 end
