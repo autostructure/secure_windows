4e338b57e830b3ff27b3dd037df38d4c5215d702
Luther <luther@autostructure.io>
2018-06-20 10:53:39 -0400

    khjghf

--- lib/facter/current_date.rb
+++ lib/facter/current_date.rb
@@ -4,7 +4,7 @@ Facter.add('current_date') do
   confine operatingsystem: :windows
   setcode do
     powershell = 'C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe'
-    command = "Get-Date -Format g"
+    command = 'Get-Date -Format g'
     currdate = Facter::Core::Execution.execute("#{command}")
     currdate.match(/........./)
   end
