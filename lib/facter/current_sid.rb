edee233cffa6eb84dea99590b2e28106b3b5e276
Luther <luther@autostructure.io>
2018-06-21 16:20:49 -0400

    beginning HKCU workaround v1.0

---
+++ lib/facter/current_sid.rb
@@ -0,0 +1,15 @@
+#
+# This fact returns the SID of the current user
+#
+
+Facter.add('current_sid') do
+  confine operatingsystem: :windows
+  setcode do
+    powershell = 'C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe'
+    command = 'Add-Type -AssemblyName System.DirectoryServices.AccountManagement'
+    Facter::Core::Execution.exec(%(#{powershell} -command "#{command}"))
+    command = 'return ([System.DirectoryServices.AccountManagement.UserPrincipal]::Current).SID.Value'
+    Facter::Core::Execution.exec(%(#{powershell} -command "#{command}"))
+
+  end
+end
