e896b9b84cb54dd8910c305f92226aabf2b85158
Luther <luther@autostructure.io>
2018-06-22 12:12:11 -0400

    new format

--- lib/facter/current_sid.rb
+++ lib/facter/current_sid.rb
@@ -6,11 +6,11 @@ Facter.add('current_sid') do
   confine operatingsystem: :windows
   setcode do
     powershell = 'C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe'
-    command = 'Add-Type -AssemblyName System.DirectoryServices.AccountManagement'
-    Facter::Core::Execution.exec(%(#{powershell} -command "#{command}"))
-    #command = 'return ([System.DirectoryServices.AccountManagement.UserPrincipal]::Current).SID.Value'
-    command = '[System.DirectoryServices.AccountManagement.UserPrincipal]::Current.SID.Value'
+    command = 'Add-Type -AssemblyName System.DirectoryServices.AccountManagement ; return ([System.DirectoryServices.AccountManagement.UserPrincipal]::Current).SID.Value'
     sid = Facter::Core::Execution.exec(%(#{powershell} -command "#{command}"))
+    #command = 'return ([System.DirectoryServices.AccountManagement.UserPrincipal]::Current).SID.Value'
+    #command = '[System.DirectoryServices.AccountManagement.UserPrincipal]::Current.SID.Value'
+    #sid = Facter::Core::Execution.exec(%(#{powershell} -command "#{command}"))
     Puppet.debug "FINDME! current_sid #{sid}"
     sid
   end
