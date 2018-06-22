b5fa7a6b895c3f8d8957cb52219fb795f9d8b49b
Luther <luther@autostructure.io>
2018-06-22 11:22:47 -0400

    debugging

--- lib/facter/current_sid.rb
+++ lib/facter/current_sid.rb
@@ -7,8 +7,10 @@ Facter.add('current_sid') do
   setcode do
     powershell = 'C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe'
     command = 'Add-Type -AssemblyName System.DirectoryServices.AccountManagement'
-    Facter::Core::Execution.execute("#{powershell} -command \"#{command}\"")
+    Facter::Core::Execution.exec(%(#{powershell} -command "#{command}"))
     command = 'return ([System.DirectoryServices.AccountManagement.UserPrincipal]::Current).SID.Value'
-    Facter::Core::Execution.execute("#{powershell} -command \"#{command}\"")
+    sid = Facter::Core::Execution.exec(%(#{powershell} -command "#{command}"))
+    Puppet.debug "#{sid}"
+    sid
   end
 end
