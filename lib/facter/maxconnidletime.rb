f836af223df5397735c00f40fca26176c06b7aaf
Luther <luther@autostructure.io>
2018-06-25 16:36:29 -0400

    refining maxconnidletime.rb

--- lib/facter/maxconnidletime.rb
+++ lib/facter/maxconnidletime.rb
@@ -4,7 +4,7 @@ Facter.add('maxconnidletime') do
   confine operatingsystem: :windows
   setcode do
     if Facter.value(:windows_server_type) == 'windowsdc'
-      time = Facter::Core::Execution.execute('dsquery * "cn=Default Query Policy,cn=Query-Policies,cn=Directory Service,cn=Windows NT,cn=Services,cn=Configuration,dc=example,dc=com" -attr LDAPAdminLimits')
+      time = Facter::Core::Execution.execute('dsquery * "cn=Default Query Policy,cn=Query-Policies,cn=Directory Service,cn=Windows NT,cn=Services,cn=Configuration,dc=test,dc=local" -attr LDAPAdminLimits')
       time.match(%r{MaxConnIdleTime=(\d*)})[1]
     end
   end
