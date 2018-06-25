e6edcbb0676a35e3d037ee50f255d10e715ea2c4
Luther <luther@autostructure.io>
2018-06-25 09:19:53 -0400

    v36776

--- manifests/stig/v36776.pp
+++ manifests/stig/v36776.pp
@@ -3,13 +3,10 @@
 class secure_windows::stig::v36776 (
   Boolean $enforced = true,
 ) {
-  # if $enforced {
-  #   registry::value { 'v36776':
-  #     key   => 'HKEY_CURRENT_USER\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\PushNotifications',
-  #     value => 'NoCloudApplicationNotification',
-  #     type  => 'dword',
-  #     data  => '0x00000001',
-  #   }
-  # }
-  #hkcu not supported :(
+  registry::value { 'v36776':
+    key   => "HKEY_USERS\\${facts['current_sid']}\\Software\\Policies\\Microsoft\\Windows\\CurrentVersion\\PushNotifications",
+    value => 'NoCloudApplicationNotification',
+    type  => 'dword',
+    data  => '1',
+  }
 }
