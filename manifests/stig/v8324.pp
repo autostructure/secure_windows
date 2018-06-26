866abe73cf6233185d5b83d9a2be7a3900d90073
Luther <luther@autostructure.io>
2018-06-26 09:18:40 -0400

    fixing v8324

--- manifests/stig/v8324.pp
+++ manifests/stig/v8324.pp
@@ -10,12 +10,17 @@ class secure_windows::stig::v8324 (
         '2'    => '0x00000002',
         '3' => '0x00000003',
       }
-      registry::value { 'v8324':
-        key   => 'HKEY_LOCAL_MACHINE\System\CurrentControlSet\Services\W32Time\TimeProviders\NtpClient',
-        value => 'Enabled',
-        type  => 'dword',
-        data  => $ntp_logging_enabled_value,
+      registry_value { 'HKEY_LOCAL_MACHINE\System\CurrentControlSet\Services\W32Time\TimeProviders\NtpClient\Enabled':
+        ensure => present,
+        type   => 'dword',
+        data   => $ntp_logging_enabled_value,
+      }
+
+      # registry::value { 'v8324':
+      #   key   => 'HKEY_LOCAL_MACHINE\System\CurrentControlSet\Services\W32Time\TimeProviders\NtpClient',
+      #   value => 'Enabled',
+      #   type  => 'dword',
+      #   data  => $ntp_logging_enabled_value,
       }
     }
   }
-}
