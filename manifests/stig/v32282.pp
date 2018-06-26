c8376bbd05d02a1220035a45be5fccf100e9c018
Luther <luther@autostructure.io>
2018-06-26 16:02:16 -0400

    4.0

--- manifests/stig/v32282.pp
+++ manifests/stig/v32282.pp
@@ -4,11 +4,11 @@ class secure_windows::stig::v32282 (
   Boolean $enforced = true,
 ) {
   if $enforced {
-    # $reg_acls = [
+     #$reg_acls = [
     #   'hklm:software\\microsoft\\active setup\\installed components',
     #   'hklm:software\\Wow6432Node\\microsoft\\active setup\\installed components',
     # ]
-    reg_acl { 'hklm:software\microsoft\active setup\installed components''hklm:software\Wow6432Node\microsoft\active setup\installed components',
+    reg_acl { 'hklm:software\\microsoft\\active setup\\installed components':
       inherit_from_parent => true,
       owner               => 'S-1-5-18',
       permissions         => [
@@ -85,5 +85,84 @@ class secure_windows::stig::v32282 (
         'PropagationFlags'  => 'InheritOnly'
       }],
     }
+
+    reg_acl { 'hklm:software\\Wow6432Node\\microsoft\\active setup\\installed components':
+      inherit_from_parent => true,
+      owner               => 'S-1-5-18',
+      permissions         => [
+      {
+        'RegistryRights'    => 'ReadKey',
+        'AccessControlType' => 'Allow',
+        'IdentityReference' => 'S-1-5-32-545',
+        'IsInherited'       => false,
+        'InheritanceFlags'  => 'None',
+        'PropagationFlags'  => 'None'
+      },
+      {
+        'RegistryRights'    => 'ReadKey',
+        'AccessControlType' => 'Allow',
+        'IdentityReference' => 'S-1-5-32-545',
+        'IsInherited'       => false,
+        'InheritanceFlags'  => 'ContainerInherit',
+        'PropagationFlags'  => 'InheritOnly'
+      },
+      {
+        'RegistryRights'    => 'FullControl',
+        'AccessControlType' => 'Allow',
+        'IdentityReference' => 'S-1-5-32-544',
+        'IsInherited'       => false,
+        'InheritanceFlags'  => 'None',
+        'PropagationFlags'  => 'None'
+      },
+      {
+        'RegistryRights'    => 'FullControl',
+        'AccessControlType' => 'Allow',
+        'IdentityReference' => 'S-1-5-32-544',
+        'IsInherited'       => false,
+        'InheritanceFlags'  => 'ContainerInherit',
+        'PropagationFlags'  => 'InheritOnly'
+      },
+      {
+        'RegistryRights'    => 'FullControl',
+        'AccessControlType' => 'Allow',
+        'IdentityReference' => 'S-1-5-18',
+        'IsInherited'       => false,
+        'InheritanceFlags'  => 'None',
+        'PropagationFlags'  => 'None'
+      },
+      {
+        'RegistryRights'    => 'FullControl',
+        'AccessControlType' => 'Allow',
+        'IdentityReference' => 'S-1-5-18',
+        'IsInherited'       => false,
+        'InheritanceFlags'  => 'ContainerInherit',
+        'PropagationFlags'  => 'InheritOnly'
+      },
+      {
+        'RegistryRights'    => 'FullControl',
+        'AccessControlType' => 'Allow',
+        'IdentityReference' => 'S-1-3-0',
+        'IsInherited'       => false,
+        'InheritanceFlags'  => 'ContainerInherit',
+        'PropagationFlags'  => 'InheritOnly'
+      },
+      {
+        'RegistryRights'    => 'ReadKey',
+        'AccessControlType' => 'Allow',
+        'IdentityReference' => 'ALL APPLICATION PACKAGES',
+        'IsInherited'       => false,
+        'InheritanceFlags'  => 'None',
+        'PropagationFlags'  => 'None'
+      },
+      {
+        'RegistryRights'    => 'ReadKey',
+        'AccessControlType' => 'Allow',
+        'IdentityReference' => 'ALL APPLICATION PACKAGES',
+        'IsInherited'       => false,
+        'InheritanceFlags'  => 'ContainerInherit',
+        'PropagationFlags'  => 'InheritOnly'
+      }],
+    }
+
   }
 }
