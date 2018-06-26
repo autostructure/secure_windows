c9a53451528bee5fe60717d93199b5e2426667ca
Luther <luther@autostructure.io>
2018-06-26 14:23:23 -0400

    so close

--- lib/facter/maxconnidletime.rb
+++ lib/facter/maxconnidletime.rb
@@ -5,12 +5,9 @@ Facter.add('maxconnidletime') do
   setcode do
     if Facter.value(:windows_server_type) == 'windowsdc'
       fact = Facter.value(:domain)
-      Puppet.debug "fact = #{fact}"
       hostarray = fact.split('.')
       host = hostarray[0]
       domain = hostarray[1]
-      Puppet.debug "host = #{host}"
-      Puppet.debug "dom = #{domain}"
       time = Facter::Core::Execution.execute("dsquery * \"cn=Default Query Policy,cn=Query-Policies,cn=Directory Service,cn=Windows NT,cn=Services,cn=Configuration,dc=#{host},dc=#{domain}\" -attr LDAPAdminLimits")
       time.match(%r{MaxConnIdleTime=(\d*)})[1]
     end
