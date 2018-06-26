957aba0d99d3ea7e5ce896c824fd97d343b1e15d
Luther <luther@autostructure.io>
2018-06-26 11:58:36 -0400

    changed 3338 if statement to registry_value

--- manifests/stig/v3338.pp
+++ manifests/stig/v3338.pp
@@ -7,12 +7,18 @@ class secure_windows::stig::v3338 (
 {
   if $enforced {
     if($facts['windows_server_type'] == 'windowsdc') {
-      registry::value { 'v3338':
-        key   => 'HKEY_LOCAL_MACHINE\System\CurrentControlSet\Services\LanManServer\Parameters',
-        value => 'NullSessionPipes',
-        type  => 'array',
-        data  => 'netlogon, samr, lsarpc',
-      }
+      registry_value { 'HKEY_LOCAL_MACHINE\System\CurrentControlSet\Services\LanManServer\Parameters\NullSessionPipes':
+        ensure => present,
+        type   => 'array',
+        data   => 'netlogon, samr, lsarpc',
+          }
+
+      # registry::value { 'v3338':
+      #   key   => 'HKEY_LOCAL_MACHINE\System\CurrentControlSet\Services\LanManServer\Parameters',
+      #   value => 'NullSessionPipes',
+      #   type  => 'array',
+      #   data  => 'netlogon, samr, lsarpc',
+      # }
     }
     else {
       registry_value { 'HKEY_LOCAL_MACHINE\System\CurrentControlSet\Services\LanManServer\Parameters\NullSessionPipes':
